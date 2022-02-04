a1cc62b983698ba1d6940b5853b615a5f9cf3623
Abhishek Gunasekar <agunase@purdue.edu>
2022-02-04 14:27:22 -0500

    Time: 45 ms (36.03%), Space: 13.9 MB (90.29%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -7,33 +7,31 @@ class Solution:
         num_list = list(str(num))
         ms = 0
         N = len(num_list)
+        
         for idx,num in enumerate(num_list):
             heapq.heappush(heap, -1*(int(num)*10 + N - idx))
         
         while len(heap) > 0:
             item = heapq.heappop(heap)
             idx,num = N - (-item % 10), (-item // 10)
-            print(idx, num, ms)
             if idx == ms:
                 ms += 1
                 continue
             else:
                 break
+                
         if len(heap) == 0:
             return int(''.join(num_list))
         else:
+            # Special case: dups allowed --> update idx
             while len(heap) != 0:
-                
                 item1 = heapq.heappop(heap)
-                print(N - (-item1 % 10))
-                print(ms, idx)
-                print(item1)
                 if (-item // 10) == (-item1 // 10):
                     idx = (N - (-item1 % 10))
                 else:
                     break
+            # swap
             num_list[ms], num_list[idx] = num_list[idx], num_list[ms]
-            print(int(''.join(num_list)))
             return int(''.join(num_list))
             
             
