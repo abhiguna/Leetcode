08da3354f75ca1e06f82fec51317bbc9e64d8661
Abhishek Gunasekar <agunase@purdue.edu>
2022-02-04 14:18:16 -0500

    Time: 48 ms (28.99%), Space: 13.9 MB (97.67%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,57 +1,39 @@
-from collections import defaultdict
+import heapq
 
 class Solution:
     def maximumSwap(self, num: int) -> int:
+        heap = []
+        heapq.heapify(heap)
         num_list = list(str(num))
-        pos_swap = 0
-        max_idx = -1
-        max_val = '0'
-        swap_found = False
+        ms = 0
+        N = len(num_list)
+        for idx,num in enumerate(num_list):
+            heapq.heappush(heap, -1*(int(num)*10 + N - idx))
         
-        i = 0
-        while i < len(num_list) - 1:
-            if num_list[i] > num_list[i+1]:
-                pos_swap = i + 1
-            elif num_list[i] < num_list[i+1]:
+        while len(heap) > 0:
+            item = heapq.heappop(heap)
+            idx,num = N - (-item % 10), (-item // 10)
+            print(idx, num, ms)
+            if idx == ms:
+                ms += 1
+                continue
+            else:
                 break
-            i += 1
-            print(pos_swap)
-            
-        if i == len(num_list) - 1:
+        if len(heap) == 0:
             return int(''.join(num_list))
-        if num_list[pos_swap] > num_list[i]:
-            pos_swap = i
-        i += 1
-        max_idx = i
-        max_val = num_list[i]
-        
-        while i < len(num_list):
-            if num_list[i] >= max_val:
-                max_idx = i
-                max_val = num_list[i]
-            i += 1
-            
-        if max_val > num_list[0]:
-            num_list[0], num_list[max_idx] = num_list[max_idx], num_list[0]
         else:
-            num_list[pos_swap], num_list[max_idx] =num_list[max_idx], num_list[pos_swap]
-        return int(''.join(num_list))    
-        
-#         for i in range(0,len(num_list)-1):
-#             if not swap_found and num_list[i] < num_list[i+1]:
-#                 pos_swap += 1
-#                 swap_found = True
-#             if not swap_found and num_list[i] == num_list[i+1]:
+            while len(heap) != 0:
                 
-#             if swap_found and i != pos_swap:
-#                 if num_list[i] >= max_val:
-#                     max_val = num_list[i]
-#                     max_idx = i
-#         print(swap_found, pos_swap)
-#         if swap_found and num_list[-1] >= max_val:
-            
-#             max_val = num_list[-1]
-#             max_idx = -1
-        
+                item1 = heapq.heappop(heap)
+                print(N - (-item1 % 10))
+                print(ms, idx)
+                print(item1)
+                if (-item // 10) == (-item1 // 10):
+                    idx = (N - (-item1 % 10))
+                else:
+                    break
+            num_list[ms], num_list[idx] = num_list[idx], num_list[ms]
+            print(int(''.join(num_list)))
+            return int(''.join(num_list))
             
             
