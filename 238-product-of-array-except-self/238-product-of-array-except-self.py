dad1553f53eb4e4c6df28cb681f1ab69d0bfa14f
Abhishek Gunasekar <agunase@purdue.edu>
2022-02-06 13:09:16 -0500

    Time: 236 ms (86.10%), Space: 21.1 MB (81.97%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -4,7 +4,7 @@ class Solution:
     # Time = O(n), Space = O(1)
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
-        answer = [0 for _ in range(N)]
+        answer = [0]*N
         answer[0] = 1
         
         for i in range(1, N):
