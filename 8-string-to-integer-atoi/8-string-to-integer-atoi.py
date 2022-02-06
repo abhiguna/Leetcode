d883b6917a386badbeea46420007c40a0a30db3f
Abhishek Gunasekar <agunase@purdue.edu>
2022-02-06 17:09:28 -0500

    Time: 41 ms (58.59%), Space: 14 MB (92.04%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,43 @@
+class Solution:
+    # Time = O(n)
+    # Space = O(1)
+    def myAtoi(self, s: str) -> int:
+        sign = 1
+        result = 0
+        idx = 0
+        N = len(s)
+        
+        INT_MAX = pow(2, 31) - 1
+        INT_MIN = -pow(2, 31)
+        
+        # Discard all spaces from beginning of the string
+        while idx < N and s[idx] == ' ':
+            idx += 1
+        
+        # Update the sign of the number. 
+        if idx < N and s[idx] == '-':
+            sign = -1
+            idx += 1
+        elif idx < N and s[idx] == '+':
+            sign = 1
+            idx += 1
+        
+        # Traverse the string until a non-digit character is encountered
+        while idx < N and s[idx].isdigit():
+            digit = int(s[idx])
+            
+            # Check overflow and underflow conditions
+            if ((result > INT_MAX // 10) or (result == INT_MAX // 10 and digit > INT_MAX % 10)):
+                # Return number based on overflow or underflow
+                return INT_MAX if sign == 1 else INT_MIN
+            
+            # Append current digit
+            result = result * 10 + digit
+            idx += 1
+        
+        # Return final result number generated from the string
+        return sign * result
+                
+        
+        
+            
