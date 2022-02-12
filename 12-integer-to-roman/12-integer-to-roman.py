fe41c42914fa4d32ffcb1b402b646bd3f021dcd9
Abhishek Gunasekar <agunase@purdue.edu>
2022-02-12 08:02:56 -0600

    Time: 62 ms (57.67%), Space: 13.8 MB (96.25%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -3,32 +3,18 @@ class Solution:
     # Time: O(1)
     # Space: O(1)
     def intToRoman(self, num: int) -> str:
-        rom_dict = {
-            'I': 1,
-            'IV': 4,
-            'V': 5,
-            'IX': 9,
-            'X': 10,
-            'XL': 40,
-            'L': 50,
-            'XC': 90,
-            'C': 100,
-            'CD': 400,
-            'D': 500,
-            'CM': 900,
-            'M': 1000
-        }
-        
-        rom_arr = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+        rom_list = [
+                    ['I',1], ['IV',4], ['V',5], 
+                    ['IX',9], ['X',10], ['XL',40], 
+                    ['L',50], ['XC',90], ['C',100], 
+                    ['CD',400], ['D',500], ['CM',900], ['M',1000]
+                   ]
         
         rom_str = []
-        divisor = len(rom_arr) - 1
         
-        while num != 0:
-            while divisor >= 0 and num // (rom_dict[rom_arr[divisor]]) == 0:
-                divisor -= 1
-            while num // (rom_dict[rom_arr[divisor]]) != 0:
-                rom_str.append(rom_arr[divisor])
-                num -= rom_dict[rom_arr[divisor]]
-            num %= rom_dict[rom_arr[divisor]]
+        for rom, val in reversed(rom_list):
+            if num // val:
+                count = num // val
+                rom_str.append(rom * count)
+                num = num % val
         return ''.join(rom_str)
