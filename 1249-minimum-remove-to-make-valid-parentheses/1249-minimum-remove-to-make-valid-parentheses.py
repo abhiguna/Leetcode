ba47b6d2adb6bbb272e1295762d93ed43032fe45
Abhishek Gunasekar <agunase@purdue.edu>
2022-03-04 15:27:42 -0500

    Time: 198 ms (33.42%), Space: 15.7 MB (77.07%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,46 @@
+from collections import deque
+
 class Solution:
+    # stack -> store (open, idx)
+    # str_to_list -> invalid idx = ""
+    # paran_mapping
+    # cases:
+    #1. close but no open before -> mark idx 
+    # 2. stack not empty --> mark all invalid idx
+    # 3. not matching opening/closing --> mark invalid closing
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = []
-        valid = []
-        N = len(s)
-        j = 0
-        for i in range(N):
-            if s[i].isalpha():
-                valid.append(s[i])
-                j += 1
-            elif s[i] == '(':
-                stack.append(('(', j))
-                valid.append('(')
-                j += 1
-            else:
-                if not stack:
-                    continue
-                stack.pop()
-                valid.append(')')
-                j += 1
-        while stack:
-            paran = stack.pop()
-            valid[paran[1]] = ''  
-        return "".join(valid)
+        paran_mapping = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+        stack = deque()
+        s = list(s)
+        for idx, char in enumerate(s):
+            if char in paran_mapping.values():
+                # open paran
+                stack.append((char, idx))
                 
+            elif char in paran_mapping.keys():
+                # close paran
+                if not stack:
+                    s[idx] = ""
+                    continue
+                    
+                top_char, top_idx = stack.pop()
+                if top_char != paran_mapping[char]:
+                    s[idx] = ""
+                    stack.append((top_char, top_idx))
+                    
+        while stack:
+            remove_char, remove_idx = stack.pop()
+            s[remove_idx] = ""
+        
+        return "".join(s)
+                    
+                
+                
+                
+                
+        
         
