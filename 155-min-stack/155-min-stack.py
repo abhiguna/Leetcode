dc33f6a53ba4660971b5df5759f8d7cba8ffe238
Abhishek Gunasekar <agunase@purdue.edu>
2022-03-04 13:31:55 -0500

    Time: 80 ms (68.16%), Space: 17.7 MB (96.78%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,37 +1,28 @@
-# Date: 2/13/22
-# 15m 5
+from collections import deque
 
 class MinStack:
-    # Time = O(1)
-    # Space = O(1)
+
     def __init__(self):
-        self.main_stack = []
-        self.min_stack = []
-        
-    # Time = O(1)
-    # Space = O(1)
+        self.stack = deque()
+        self.min_list = [float('inf')]
+
     def push(self, val: int) -> None:
-        self.main_stack.append(val)
-        if not self.min_stack or self.min_stack[-1] > val:
-            self.min_stack.append(val)
-        else:
-            self.min_stack.append(self.min_stack[-1])
-
-    # Time = O(1)
-    # Space = O(1)
+        self.stack.append(val)
+        if val <= self.min_list[-1]:
+            self.min_list.append(val)
+        
     def pop(self) -> None:
-        self.min_stack.pop()
-        self.main_stack.pop()
+        deleted_val = self.stack.pop()
+        if deleted_val == self.min_list[-1]:
+            self.min_list.pop()
 
-    # Time = O(1)
-    # Space = O(1)
     def top(self) -> int:
-        return self.main_stack[-1]
-
-    # Time = O(1)
-    # Space = O(1)
+        return self.stack[-1]
+        
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.min_list[-1]
+        
+        
 
 
 # Your MinStack object will be instantiated and called as such:
