1c1572b3b0e62bd015caf1fae2fb4a6b1545e290
Abhishek Gunasekar <agunase@purdue.edu>
2022-03-04 12:07:14 -0500

    Time: 36 ms (81.46%), Space: 14.2 MB (49.90%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -8,35 +8,35 @@
 from collections import deque
 
 class Solution:
+    # node_queue --> main BFS queue
+    # zig_zag --> maintain zigzag order
+    # result --> output
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        output = []
-        level_list = deque()
-        is_order_left = True
         
-        node_queue = deque([root, None]) # None is the delimeter
+        result = []
+        zig_zag = deque()
+        node_queue = deque([root, None])
+        l2r = True
+        
         while node_queue:
-            curr_node = node_queue.popleft();
+            curr_node = node_queue.popleft()
             if curr_node:
-                if is_order_left:
-                    level_list.append(curr_node.val)
+                if l2r:
+                    zig_zag.append(curr_node.val)
                 else:
-                    level_list.appendleft(curr_node.val)
+                    zig_zag.appendleft(curr_node.val)
                 
                 if curr_node.left:
                     node_queue.append(curr_node.left)
                 if curr_node.right:
                     node_queue.append(curr_node.right)
-                
             else:
-                output.append(level_list)
-                
                 if node_queue:
                     node_queue.append(None)
-                
-                level_list = deque()
-                is_order_left = not is_order_left
-        
-        return output
-                
+                l2r = not l2r
+                result.append(zig_zag)
+                zig_zag = deque()
+
+        return result
