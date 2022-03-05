c3b7dce2d1ddffe9d99734202f5ac1f66f3eb705
Abhishek Gunasekar <agunase@purdue.edu>
2022-03-05 09:54:52 -0500

    Time: 205 ms (9.72%), Space: 14.9 MB (5.39%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,33 +1,25 @@
-# Date: 2/14/22
-# 20m 3
+from collections import deque
+
 class Solution:
-    # Pattern: stack
-    # Time = O(n + k)
-    # Space = O(n)
+    # stack
+    # remove_mode
+    
     def removeKdigits(self, num: str, k: int) -> str:
-        stack = []
-        N = len(num)
-        i = 0
-        while i < N:
-            # Remove decreasing order elements
-            while stack and k > 0 and stack[-1] > num[i]:
+        stack = deque()
+        
+        if k >= len(num):
+            return "0"
+        
+        for n in num:
+            while stack and k > 0 and int(n) < int(stack[-1]):
                 stack.pop()
                 k -= 1
-            stack.append(num[i])
-            i += 1
-        # Special case: if num in increasing order
+    
+            stack.append(n)
+        
         while k > 0:
             stack.pop()
             k -= 1
         
-        min_num = "".join(stack)
-        
-        # Special Case: remove leading zeroes
-        if min_num != "0":
-            min_num = min_num.lstrip('0')
-        # Special case: empty string
-        if min_num == "":
-            return "0"
-        return min_num
-        
+        return str(int("".join(stack)))
         
