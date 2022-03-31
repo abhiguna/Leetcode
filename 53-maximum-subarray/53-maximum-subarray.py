c14292e88a8a976442e3ac795aa5b5fa1b1143ed
Abhishek Gunasekar <agunase@purdue.edu>
2022-03-31 09:11:01 -0400

    Time: 796 ms (80.40%), Space: 28.5 MB (10.05%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,40 +1,15 @@
-import math
-
 class Solution:
     
-    def max_sum_sub_rec(self, nums, start, end):
-        # Base case:
-        if end < start:
-            return -math.inf
-
-        mid = start + (end - start) // 2
-        curr_sum = 0
-        max_left_sum = 0
-        max_right_sum = 0
-        
-        # Iterate from the middle to the beginning
-        for i in range(mid - 1, start - 1, -1):
-            curr_sum += nums[i]
-            max_left_sum = max(max_left_sum, curr_sum)
-        
-        # Reset curr and iterate from the middle to the end
-        curr_sum = 0
-        for i in range(mid + 1, end + 1):
-            curr_sum += nums[i]
-            max_right_sum = max(max_right_sum, curr_sum)
-        
-        # The best combined sum uses the middle element and 
-        # the best possible sum from each half
-        best_combined_sum = max_left_sum + nums[mid] + max_right_sum
-        
-        # Find the best subarray possible from both halves.
-        left_half = self.max_sum_sub_rec(nums, start, mid - 1)
-        right_half = self.max_sum_sub_rec(nums, mid + 1, end)
-        
-        # The largest of the 3 is the answer for any given input array
-        return max(best_combined_sum, left_half, right_half)
-        
-    
+    # Time = O(N)
+    # Space = O(1)
     def maxSubArray(self, nums: List[int]) -> int:
         N = len(nums)
-        return self.max_sum_sub_rec(nums, 0, N - 1)
+        
+        curr_sum = nums[0]
+        max_sum = nums[0]
+        
+        for i in range(1, N):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
