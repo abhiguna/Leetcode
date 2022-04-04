f4479b52fbe1459d4b52408ec8f9ac9c3d029bb6
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-04 13:43:30 -0400

    Time: 8267 ms (5.02%), Space: 14.3 MB (51.49%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,19 +1,18 @@
 class Solution:
-    # Time = O(n^2)
-    # Space = O(n)
+    
+    # Time = O(N^2)
+    # Space = O(N)
+    
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def getMax(dp, nums, n):
-            max_len = 0
-            target = nums[n]
-            for i in range(n):
-                if nums[i] < target:
+        N = len(nums)
+        
+        dp = [1] * N
+        max_len = 1
+        
+        for i in range(N - 1, -1, -1):
+            for j in range(i + 1, N):
+                if nums[j] > nums[i]:
+                    dp[i] = max(dp[i], 1 + dp[j])
                     max_len = max(max_len, dp[i])
-            return max_len
-            
-        dp = [1 for _ in nums]
-        longest = 1
-        for i in range(len(nums)):
-            prev_long = getMax(dp, nums, i)
-            dp[i] = max(dp[i], prev_long + 1)
-            longest = max(longest, prev_long + 1)
-        return longest
+        
+        return max_len
