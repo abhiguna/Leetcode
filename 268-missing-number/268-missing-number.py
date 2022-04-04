42733689e9c408f4d1562ca8340d888069b4d093
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-04 09:20:03 -0400

    Time: 229 ms (35.68%), Space: 15.2 MB (37.80%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -3,7 +3,11 @@ class Solution:
     # Time = O(N)
     # Space = O(1)
     def missingNumber(self, nums: List[int]) -> int:
-        total_sum = sum(range(len(nums) + 1))
-        actual_sum = sum(nums)
-        return total_sum - actual_sum
+        N = len(nums)
+        missing_num = N
+        
+        for idx, num in enumerate(nums):
+            missing_num ^= idx ^ num
+        
+        return missing_num
         
