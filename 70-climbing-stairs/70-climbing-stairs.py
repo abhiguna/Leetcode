610b3f34f829c3be2aea23422c711e161d698548
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-04 11:59:42 -0400

    Time: 30 ms (87.55%), Space: 13.8 MB (62.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -6,9 +6,12 @@ class Solution:
         if n == 1:
             return 1
         
-        opt = [1] * (n + 1)
+        first = 1
+        second = 2
         
-        for i in range(2, n + 1):
-            opt[i] = opt[i-1] + opt[i-2]
-        
-        return opt[n]
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+            
+        return second
