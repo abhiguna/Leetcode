fb2ee1cba32bbb296a30201fdc253bd2d3fa2e23
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-04 09:16:22 -0400

    Time: 136 ms (92.22%), Space: 15.1 MB (98.48%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -3,23 +3,7 @@ class Solution:
     # Time = O(N)
     # Space = O(1)
     def missingNumber(self, nums: List[int]) -> int:
-        N = len(nums)
-        
-        # Cyclic sort
-        i = 0
-        while i < N:
-            swap_idx = nums[i]
-            
-            if nums[i] < N and nums[i] != nums[swap_idx]:
-                # Swap values
-                nums[i], nums[swap_idx] = nums[swap_idx], nums[i]
-            else:
-                i += 1
-        
-        # Iterate to find missing number
-        for i in range(N):
-            if nums[i] != i:
-                return i
-        
-        return N
+        total_sum = sum(range(len(nums) + 1))
+        actual_sum = sum(nums)
+        return total_sum - actual_sum
         
