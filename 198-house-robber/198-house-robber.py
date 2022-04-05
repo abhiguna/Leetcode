0c00ba7d67cac590acc5e3ccc850ca7918781311
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-05 09:53:49 -0400

    Time: 39 ms (68.31%), Space: 13.9 MB (70.52%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,18 +1,16 @@
-# Date: 2/12/22
-# 30m 5
 class Solution:
-    # Pattern: dynamic programming
-    # Time = O(n)
+    # Time = O(N)
     # Space = O(1)
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
-        no_rob = 0
-        rob = nums[0]
-        for i in range(1, N):
-            prev_rob = rob
-            rob = nums[i] + no_rob
-            no_rob = max(no_rob, prev_rob)
-        return max(rob, no_rob)
-    
-    
         
+        # Base case
+        first = 0
+        second = nums[0]
+        
+        for i in range(1, N):
+            third = max(nums[i] + first, second)
+            first = second
+            second = third
+        
+        return second
