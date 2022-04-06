88f91685626f515acb7e8fa3b0bcfa4170e06069
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-06 09:35:20 -0400

    Time: 56 ms (98.27%), Space: 15.4 MB (14.84%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,16 +1,15 @@
 from collections import *
 
 class Solution:
-    # Time = O(N)
-    # Space = O(N)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         N = len(nums)
-        res = [-1, -1]
         nums_seen = defaultdict(int)
         
+        res = [-1, -1]
+        
         for i in range(N):
-            if target - nums[i] in nums_seen:
-                res[0], res[1] = nums_seen[target-nums[i]], i
+            if (target - nums[i]) in nums_seen:
+                res = [nums_seen[target-nums[i]], i]
                 break
             
             nums_seen[nums[i]] = i
