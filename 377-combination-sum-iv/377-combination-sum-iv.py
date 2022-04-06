83c73b59d8d27090c6aabdc0fbbee6576e004932
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-06 09:30:45 -0400

    Time: 68 ms (30.90%), Space: 13.9 MB (50.63%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,22 +1,17 @@
 class Solution:
     
-    # Time = O(N * M)
+    # Time = O(target*N), N = len(nums)
     # Space = O(N)
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        N = target
-        M = len(nums)
-        
-        dp = [0 for i in range(N+1)]
+        N = len(nums)
+        dp = [0] * (target + 1)
         # Base
         dp[0] = 1
         
-        for i in range(1, N+1):
-            combinations = 0
-            
+        for target in range(1, target+1):
             for num in nums:
-                if i >= num:
-                    combinations += dp[i - num]
-            
-            dp[i] = combinations
+                if target >= num:
+                    dp[target] += dp[target-num]
         
-        return dp[N]
+        return dp[target]
+                    
