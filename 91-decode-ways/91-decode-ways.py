3549f9bd6474c867a2f0c33d91759eb677099289
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-07 08:42:48 -0400

    Time: 36 ms (82.43%), Space: 13.9 MB (82.76%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,21 +1,24 @@
+from collections import *
+
 class Solution:
+    
     # Time = O(N)
     # Space = O(N)
     def numDecodings(self, s: str) -> int:
-        # Memoized results
         N = len(s)
-        dp = {N : 1}
+        dp = defaultdict(int)
+        dp[N] = 1
         
-        for i in range(N-1, -1 , -1):
+        for i in range(N-1, -1, -1):
             if s[i] == "0":
                 dp[i] = 0
             else:
-                # Only one character taken
+                # Take one character
                 dp[i] = dp[i+1]
             
-            # Check if possible to take two characters
-            if i+1 < N and (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456")):
+            # Take two characters if possible
+            if (i < N-1) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
                 dp[i] += dp[i+2]
         
         return dp[0]
-            
+                
