e3907937e56606353eae43150df8f7d6778ad246
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-07 08:18:57 -0400

    Time: 980 ms (48.02%), Space: 28.3 MB (18.12%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,14 +1,14 @@
 class Solution:
-    
     # Time = O(N)
     # Space = O(1)
     def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's algorithm
         N = len(nums)
         curr_sum = nums[0]
-        max_sum = curr_sum
+        max_sum = max(nums)
         
         for i in range(1, N):
-            curr_sum = max(nums[i], curr_sum + nums[i])
+            curr_sum = max(curr_sum + nums[i], nums[i])
             max_sum = max(max_sum, curr_sum)
-        
+            
         return max_sum
