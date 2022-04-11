60604cd2a346b1f0206cb99334cc52669f6f065b
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-10 22:53:34 -0400

    Time: 36 ms (94.11%), Space: 13.9 MB (41.93%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    # Time = O(M + N)
+    # Space = O(1)
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        write_idx = m + n - 1
+        
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[write_idx] = nums1[i]
+                i -= 1
+            else:
+                nums1[write_idx] = nums2[j]
+                j -= 1
+            
+            write_idx -= 1
+        
+        while i >= 0:
+            nums1[write_idx] = nums1[i]
+            i -= 1
+            write_idx -= 1
+        
+        while j >= 0:
+            nums1[write_idx] = nums2[j]
+            j -= 1
+            write_idx -= 1
+        
+        return
+        
