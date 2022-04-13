1570856e84ab7745255167f52555cbd4dbbfb8e9
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-13 17:22:58 -0400

    Time: 1281 ms (26.16%), Space: 20.2 MB (93.48%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,29 +1,46 @@
-from collections import defaultdict
-import heapq
+import math
+import random
 
 class Solution:
-    # Time = O(n + klogn)
-    # Space = O(n)
-    
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def calcDist(point):
-            return math.sqrt(point[0]**2 + point[1]**2)
+    def get_distance(self, point):
+        return math.sqrt((point[0] ** 2) + (point[1] ** 2))
         
-        point_dist = []
-        lookup = defaultdict(list)
-        for point in points:
-            dist_from_or = calcDist(point)
-            point_dist.append(dist_from_or)
-            lookup[dist_from_or].append(point)
-        k_closest = []
-        heapq.heapify(point_dist)
-        i = 0
-        while i < k:
-            min_dist = heapq.heappop(point_dist)
-            points = lookup[min_dist]
-            for point in points:
-                if i == k:
-                    break
-                k_closest.append(point)
-                i += 1
-        return k_closest
+    def partition(self, points, start, end):
+        pivot_idx = random.randint(start, end)
+        # Swap
+        points[start], points[pivot_idx] = points[pivot_idx], points[start]
+        smaller = start
+        for bigger in range(start + 1, end + 1):
+            if self.get_distance(points[bigger]) <= self.get_distance(points[start]):
+                smaller += 1
+                # Swap
+                points[smaller], points[bigger] = points[bigger], points[smaller]
+        # Swap with pivot_idx
+        points[start], points[smaller] = points[smaller], points[start]
+        return smaller
+        
+    def helper(self, points, start, end, target_idx):
+        # Base case
+        if start == end:
+            return
+        
+        pivot_idx = self.partition(points, start, end)
+        if pivot_idx == target_idx:
+            return
+        elif pivot_idx < target_idx:
+            self.helper(points, pivot_idx + 1, end, target_idx)
+        else:
+            self.helper(points, start, pivot_idx - 1, target_idx)
+    
+    # Time = O(N)
+    # Space = O(1)
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        N = len(points)
+        # Edge case
+        if N == 1:
+            return points
+        
+        self.helper(points, 0, N - 1, k - 1)
+        return points[:k]
+        
+        
