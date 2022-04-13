76f6304980fcd78dcb89778d44e81443d66eafc7
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-13 17:33:41 -0400

    Time: 40 ms (72.17%), Space: 13.9 MB (67.60%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,38 +1,24 @@
-# Not Optimal
-
-# Date: 2/6/22
-# 30m 5
 class Solution:
-    # Pattern = ad-hoc
-    # Time = O(n)
+    # Time = O(N)
     # Space = O(1)
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        num_red = 0
-        num_white = 0
-        num_blue = 0
-        for color in nums:
-            if color == 0:
-                num_red += 1
-            elif color == 1:
-                num_white += 1
-            else:
-                num_blue += 1
         N = len(nums)
-        idx = 0
-        while idx<N and num_red>0:
-            nums[idx] = 0
-            idx += 1
-            num_red -= 1
-        while idx<N and num_white>0:
-            nums[idx] = 1
-            idx += 1
-            num_white -= 1
-        while idx<N and num_blue>0:
-            nums[idx] = 2
-            idx += 1
-            num_blue -= 1    
-        return
+        red = 0
+        blue = N - 1
+        read = 0
+        
+        while read <= blue:
+            if nums[read] == 0:
+                # Swap with red
+                nums[red], nums[read] = nums[read], nums[red]
+                red += 1
+                read += 1
+            elif nums[read] == 1:
+                read += 1
+            else:
+                # Swap with blue
+                nums[blue], nums[read] = nums[read], nums[blue]
+                blue -= 1
+        
+        return nums
         
