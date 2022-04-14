83ee060a00bffce157402a25e46c841d74144694
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-14 19:23:20 -0400

    Time: 83 ms (85.29%), Space: 17.5 MB (52.01%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,26 +1,26 @@
+from heapq import *
+
 class Solution:
-    # Time = O(nlogn)
-    # Space = O(n)
+    # Time = O(NlogN)
+    # Space = O(N)
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         N = len(intervals)
-        start_times = [time[0] for time in intervals]
-        end_times = [time[1] for time in intervals]
+        intervals.sort(key = lambda x: x[0])
+        min_heap = []
+        min_rooms = 0
         
-        start_times.sort()
-        end_times.sort()
-        
-        rooms = 0
-        max_rooms = rooms
-        start_idx = 0
-        end_idx = 0
-        
-        while start_idx < N:
-            if start_times[start_idx] < end_times[end_idx]:
-                rooms += 1
-                max_rooms = max(max_rooms, rooms)
-                start_idx += 1
+        for i in range(N):
+            if i == N - 1:
+                next_start = float("inf")
             else:
-                rooms -= 1
-                end_idx += 1
+                next_start = intervals[i+1][0]
+            
+            # Start
+            heappush(min_heap, intervals[i][1])
+            min_rooms = max(min_rooms, len(min_heap))
+            
+            # End 
+            while min_heap and min_heap[0] <= next_start:
+                heappop(min_heap)
         
-        return max_rooms
+        return min_rooms
