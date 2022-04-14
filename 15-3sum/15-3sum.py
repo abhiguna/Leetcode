70d6a7170b6482681534745a3804367daca77dc3
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-14 18:31:11 -0400

    Time: 801 ms (85.10%), Space: 18.1 MB (44.00%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,44 +1,41 @@
 class Solution:
     
-    def two_sum(self, nums, N, i, res):
+    def find_pairs(self, nums, i, N, triplets):
         j = i + 1
         k = N - 1
-        
         while j < k:
-            _sum = nums[i] + nums[j] + nums[k]
-            
-            if _sum < 0:
+            curr_sum = nums[i] + nums[j] + nums[k]
+            if curr_sum < 0:
                 j += 1
-            elif _sum > 0:
+            elif curr_sum > 0:
                 k -= 1
             else:
-                res.append([nums[i], nums[j], nums[k]])
+                # Equal
+                triplets.append([nums[i], nums[j], nums[k]])
                 
                 j += 1
-                # Prevent duplicates for second ptr
-                while j < k and nums[j] == nums[j - 1]:
+                while j < k and nums[j] == nums[j-1]:
                     j += 1
-                
+                    
                 k -= 1
-                # Prevent duplicates for third ptr
-                while j < k and nums[k] == nums[k + 1]:
+                while j < k and nums[k] == nums[k+1]:
                     k -= 1
-        
-        return res
-                
+        return
+    
     # Time = O(N^2)
     # Space = O(N)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
         nums.sort()
-        
-        res = []
-        
+        N = len(nums)
+        triplets = []
+        i = 0
         for i in range(N - 2):
-            # Prevent duplicates for first ptr
-            if i > 0 and nums[i] == nums[i-1]:
+            # Skip duplicates
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
             
-            self.two_sum(nums, N, i, res)
+            self.find_pairs(nums, i, N, triplets)
         
-        return res
+        return triplets
+            
+        
