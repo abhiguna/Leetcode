8677aa138678d126a925499dbbd1e198ee8c43d5
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-14 20:21:14 -0400

    Time: 148 ms (94.29%), Space: 18 MB (98.88%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,22 +1,17 @@
 class Solution:
-    # Time = O(nlogn), Space = O(n)
+    # Time = O(NlogN)
+    # Space = O(N)
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals) == 1:
-            return intervals
-        intervals = sorted(intervals, key=lambda time: time[0])
-        curr_start = intervals[0][0]
-        curr_end = intervals[0][1]
-        i = 1
         N = len(intervals)
-        merged = []
-        while i < N:
-            if intervals[i][0] <= curr_end:
-                curr_end = max(curr_end, intervals[i][1])
+        intervals.sort(key=lambda x : x[0])
+        result = [intervals[0]]
+        
+        for i in range(1, N):
+            # Check overlap
+            if result[-1][1] >= intervals[i][0]:
+                result[-1] = [result[-1][0], max(result[-1][1], intervals[i][1])]
+            # No overlap
             else:
-                merged.append([curr_start, curr_end])
-                curr_start = intervals[i][0]
-                curr_end = intervals[i][1]
-            if i == N - 1:
-                merged.append([curr_start, curr_end])   
-            i += 1
-        return merged
+                result.append(intervals[i])
+        
+        return result
