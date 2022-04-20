8e2f2deffd2ca5ea17a4fe588d09459afc19df3a
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-20 00:30:47 -0400

    Time: 101 ms (51.01%), Space: 14.5 MB (44.89%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,30 +1,31 @@
-from heapq import *
-
 class Solution:
     # Time = O(NlogN)
     # Space = O(N)
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         trips.sort(key = lambda x: x[1])
-        N = len(trips)
-        min_heap = []
         total_passengers = 0
+        min_heap = []
+        N = len(trips)
         
         for i in range(N):
             if i == N - 1:
-                next_start = float("inf")
+                next_start = math.inf
             else:
                 next_start = trips[i+1][1]
-            
-            # Start a new trip
-            heappush(min_heap, (trips[i][2], trips[i][0]))
+                
+            # Start the current trip
             total_passengers += trips[i][0]
+            
             if total_passengers > capacity:
                 return False
             
-            # End the previous trip
+            heappush(min_heap, ((trips[i][2], trips[i][0])))
+            
+            # End the current trip
             while min_heap and min_heap[0][0] <= next_start:
-                end_time, pass_count = heappop(min_heap)
-                total_passengers -= pass_count
-        
+                end_time, passengers = heappop(min_heap)
+                total_passengers -= passengers
+                
         return True
+            
             
