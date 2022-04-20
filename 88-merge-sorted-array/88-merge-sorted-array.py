e84e8b38981a4f4a7fe389d1c6993e63ee163f6b
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-19 22:17:35 -0400

    Time: 96 ms (5.39%), Space: 13.8 MB (99.36%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,31 +1,26 @@
 class Solution:
-    
     # Time = O(M + N)
     # Space = O(1)
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = m - 1
-        j = n - 1
-        write_idx = m + n - 1
+        ptr1 = m - 1
+        ptr2 = n - 1
+        write = m + n - 1
         
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[write_idx] = nums1[i]
-                i -= 1
+        while ptr1 >= 0 and ptr2 >= 0:
+            if nums1[ptr1] > nums2[ptr2]:
+                nums1[write] = nums1[ptr1]
+                ptr1 -= 1
             else:
-                nums1[write_idx] = nums2[j]
-                j -= 1
+                nums1[write] = nums2[ptr2]
+                ptr2 -= 1
             
-            write_idx -= 1
+            write -= 1
         
-        while i >= 0:
-            nums1[write_idx] = nums1[i]
-            i -= 1
-            write_idx -= 1
-        
-        while j >= 0:
-            nums1[write_idx] = nums2[j]
-            j -= 1
-            write_idx -= 1
+        # ptr1 out of bounds
+        while ptr2 >= 0:
+            nums1[write] = nums2[ptr2]
+            ptr2 -= 1
+            write -= 1
         
         return
         
