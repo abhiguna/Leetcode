86663d60c848dbe447a57868f0cf365f70222399
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-21 20:23:36 -0400

    Time: 112 ms (36.10%), Space: 14.4 MB (48.98%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,28 +1,27 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        perm = []
-        count = {n:0 for n in nums}
-        for n in nums:
-            count[n] += 1
+        N = len(nums)
+        res = []
+        nums.sort()
         
-        def dfs():
-            if len(perm) == len(nums):
-                result.append(perm.copy())
+        def dfs(idx):
+            # Base case
+            if idx == N:
+                res.append(nums[:])
                 return
             
-            for n in count:
-                if count[n] > 0:
-                    perm.append(n)
-                    count[n] -= 1
-                    
-                    dfs()
-                    
-                    count[n] += 1
-                    perm.pop()
-                    
-        dfs()
-        return result
+            already_used = set()
+            
+            for i in range(idx, N):
+                if nums[i] not in already_used:
+                    already_used.add(nums[i])
+                    # Swap
+                    nums[idx], nums[i] = nums[i], nums[idx]
+                    dfs(idx + 1)
+                    # Swap back
+                    nums[idx], nums[i] = nums[i], nums[idx]
+            
+            return
         
-    
-        
+        dfs(0)
+        return res
