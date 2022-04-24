7cc3727249d72581463056f3d5dfe7f922ca2a3d
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-24 13:31:16 -0400

    Time: 84 ms (5.32%), Space: 14.1 MB (60.05%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -4,39 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
-
 class Solution:
-    # node_queue --> main BFS queue
-    # zig_zag --> maintain zigzag order
-    # result --> output
+    
+    # Time = O(N)
+    # Space = O(N)
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         
-        result = []
-        zig_zag = deque()
-        node_queue = deque([root, None])
-        l2r = True
+        res = []
+        q = deque([root])
+        is_lr = True
         
-        while node_queue:
-            curr_node = node_queue.popleft()
-            if curr_node:
-                if l2r:
-                    zig_zag.append(curr_node.val)
-                else:
-                    zig_zag.appendleft(curr_node.val)
+        while q:
+            num_nodes = len(q)
+            curr_level = deque()
+            
+            for i in range(num_nodes):
+                node = q.popleft()
                 
-                if curr_node.left:
-                    node_queue.append(curr_node.left)
-                if curr_node.right:
-                    node_queue.append(curr_node.right)
-            else:
-                if node_queue:
-                    node_queue.append(None)
-                l2r = not l2r
-                result.append(zig_zag)
-                zig_zag = deque()
-
-        return result
+                if is_lr:
+                    curr_level.append(node.val)
+                else:
+                    curr_level.appendleft(node.val)
+                    
+                if node.left:
+                    q.append(node.left)
+                
+                if node.right:
+                    q.append(node.right)
+            
+            res.append(curr_level)
+            is_lr = not is_lr
+        
+        return res
+        
