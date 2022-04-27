e1b939b33b3aac275aaef826484b44eb75330ee6
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-27 07:42:34 -0400

    Time: 42 ms (66.16%), Space: 14.1 MB (99.00%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,38 +1,38 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
-
 class Solution:
+    
+    # Time = O(N)
+    # Space = O(N)
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         
-        output = []
-        level_list = deque()
-        node_queue = deque([root, None])
+        res = []
+        queue = deque([root])
         
-        
-        while node_queue:
-            curr_node = node_queue.popleft()
+        while queue:
+            k = len(queue) # k: num of nodes at curr level
+            curr_level = []
             
-            if curr_node:
-                level_list.append(curr_node.val)
+            for i in range(k):
+                curr_node = queue.popleft() # FIFO
+                curr_level.append(curr_node.val)
                 
+                # Add children to queue
                 if curr_node.left:
-                    node_queue.append(curr_node.left)
+                    queue.append(curr_node.left)
+                
                 if curr_node.right:
-                    node_queue.append(curr_node.right)
-            else:
-                output.append(level_list)
-                
-                if node_queue:
-                    node_queue.append(None)
-                
-                level_list = deque()
+                    queue.append(curr_node.right)
+            
+            res.append(curr_level)
         
-        return output
+        return res
+        
