e4aeba97362b515c92f1ce7d805f70f42f08bdcd
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-28 01:08:37 -0400

    Time: 45 ms (53.01%), Space: 14 MB (49.86%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -13,35 +13,34 @@ class Solution:
     # Space = O(N)
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         parent_x = None
-        depth_x = None
-        
         parent_y = None
-        depth_y = None
         
         curr_depth = -1
-        queue = deque([(root, None)])
+        queue = deque([root])
         
         while queue:
             num_nodes = len(queue)
             curr_depth += 1
             
             for _ in range(num_nodes):
-                curr_node, parent = queue.popleft()
-                
-                if curr_node.val == x:
-                    parent_x = parent
-                    depth_x = curr_depth
-                elif curr_node.val == y:
-                    parent_y = parent
-                    depth_y = curr_depth
+                curr_node = queue.popleft()
                 
                 if curr_node.left:
-                    queue.append((curr_node.left, curr_node))
+                    queue.append(curr_node.left)
+                    if curr_node.left.val == x:
+                        parent_x = curr_node.val
+                    elif curr_node.left.val == y:
+                        parent_y = curr_node.val
                 
                 if curr_node.right:
-                    queue.append((curr_node.right, curr_node))
-            
-        if (depth_x == depth_y) and (parent_x != parent_y):
-            return True
+                    queue.append(curr_node.right)
+                    if curr_node.right.val == x:
+                        parent_x = curr_node.val
+                    elif curr_node.right.val == y:
+                        parent_y = curr_node.val
+                
+                # If either parent is found in the curr level check bool condition
+            if parent_x or parent_y:
+                return (parent_x and parent_y) and (parent_x != parent_y)
         
         return False
