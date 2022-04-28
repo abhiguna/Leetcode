c944341eddcb47e9edf3c1d4fc9930bd6f6f8684
Abhishek Gunasekar <agunase@purdue.edu>
2022-04-27 23:27:39 -0400

    Time: 44 ms (53.37%), Space: 14 MB (24.20%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,7 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
+    
+    # Time = O(N)
+    # Space = O(N)
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
@@ -13,14 +18,14 @@ class Solution:
         queue = deque([root])
         
         while queue:
-            level = []
             num_nodes = len(queue)
             
-            # Process curr level
             for i in range(num_nodes):
-                curr_node = queue.popleft()
+                curr_node = queue.popleft() 
                 
-                level.append(curr_node.val)
+                # Last node of curr level
+                if i == num_nodes - 1:
+                    res.append(curr_node.val)
                 
                 if curr_node.left:
                     queue.append(curr_node.left)
@@ -28,9 +33,5 @@ class Solution:
                 if curr_node.right:
                     queue.append(curr_node.right)
             
-            # Append rightmost node in curr level to final result
-            res.append(level[-1])
-        
         return res
-            
-        
+                    
