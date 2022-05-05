b2068c27301aee9913820151ce04abac74b7d443
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-05 06:30:32 -0400

    Time: 164 ms (30.67%), Space: 17 MB (44.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,47 +1,33 @@
 class Solution:
-    # Time = O(M + N)
-    # Space = O(M + N)
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        N = numCourses
-        adj_list = [[] for _ in range(N)]
+        # map each course to prereq list
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
         
-        def build_graph():
-            for (course, pre) in prerequisites:
-                adj_list[pre].append(course)
-            return
-        
-        build_graph()
-        
-        visited = [-1] * N
-        arrival = [-1] * N
-        departure = [-1] * N
-        timestamp = 0
-        
-        def dfs(src):
-            nonlocal timestamp
+        # visitSet = all courses taken on the curr DFS path
+        visitSet = set()
+        def dfs(crs):
+            if crs in visitSet:
+                return False
             
-            visited[src] = 1
-            arrival[src] = timestamp
-            timestamp += 1
+            if preMap[crs] == []:
+                return True
             
-            for neighbor in adj_list[src]:
-                if visited[neighbor] == -1:
-                    has_cycle = dfs(neighbor)
-                    if has_cycle:
-                        return True
-                # Back edge
-                elif departure[neighbor] == -1:
-                    return True
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
             
-            departure[src] = timestamp
-            timestamp += 1
-            return False
-                
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
         
-        for v in range(N):
-            if visited[v] == -1:
-                has_cycle = dfs(v)
-                if has_cycle:
-                    return False
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        
         return True
+    
+        # Not connected graph
+        # 1 -> 2 
+        # 3 -> 4
             
