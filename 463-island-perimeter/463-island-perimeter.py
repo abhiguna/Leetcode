d70cdc6f2156e2ef177a0da83aa6d4e9243d48cd
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-05 19:54:31 -0400

    Time: 761 ms (41.73%), Space: 28.9 MB (7.53%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,52 +1,27 @@
 class Solution:
     # Time = O(M*N)
-    # Space = O(max(M, N))
+    # Space = O(M*N)
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         M = len(grid)
         N = len(grid[0])
-        perimeter = [0]
         
-        def get_num_sides(row, col):
-            sides = 0
-            # Top side
-            if row == 0 or grid[row-1][col] == 0:
-                sides += 1
-            # Bottom side
-            if row == M-1 or grid[row+1][col] == 0:
-                sides += 1
-            # Right side
-            if col == N-1 or grid[row][col+1] == 0:
-                sides += 1
-            # Left side
-            if col == 0 or grid[row][col-1] == 0:
-                sides += 1
-            return sides
+        visited = set()
         
-        dx = [0, 0, 1, -1]
-        dy = [-1, 1, 0, 0]
-        
-        def bfs(row, col):
-            queue = deque([(row, col)])
-            visited = set([(row, col)])
-        
-            while queue:
-                curr_row, curr_col = queue.popleft()
-                perimeter[0] += get_num_sides(curr_row, curr_col)
-                
-                for i in range(4):
-                    nrow = curr_row + dy[i]
-                    ncol = curr_col + dx[i]
-                    if nrow < 0 or ncol < 0 or nrow >= M or ncol >= N or \
-                        ((nrow, ncol) in visited) or grid[nrow][ncol] == 0:
-                        continue
-                    visited.add((nrow, ncol))
-                    queue.append((nrow, ncol))
+        def dfs(row, col):
+            # Base case
+            if row < 0 or col < 0 or row >= M or col >= N or grid[row][col] == 0:
+                return 1
+            if (row, col) in visited:
+                return 0
             
+            # Recursive case
+            visited.add((row, col))
+            perimeter = dfs(row+1, col) + dfs(row-1, col) + dfs(row, col+1) + dfs(row, col-1)
             return perimeter
-        
+            
         for r in range(M):
             for c in range(N):
                 if grid[r][c] == 1:
-                    return bfs(r, c)[0]
+                    return dfs(r, c)
         
         return 0
