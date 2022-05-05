5ea3bc8b09b46226eb01f4dc883e58adf7801b23
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-05 18:57:49 -0400

    Time: 6920 ms (53.34%), Space: 14 MB (13.78%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,32 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        M = len(board)
+        N = len(board[0])
+        visited = set()
+        
+        def dfs(row, col, idx):
+            # Base case
+            if idx == len(word):
+                return True
+            if row < 0 or col < 0 or row >= M or col >= N or board[row][col] != word[idx] or \
+                (row, col) in visited:
+                return False
+            
+            visited.add((row, col))
+            res = dfs(row+1, col, idx+1) or \
+                    dfs(row-1, col, idx+1) or \
+                    dfs(row, col+1, idx+1) or \
+                    dfs(row, col-1, idx+1)
+            visited.remove((row, col))
+            return res
+                
+        for r in range(M):
+            for c in range(N):
+                word_found = dfs(r, c, 0)
+                if word_found: return True
+        
+        return False
+        
+        
+        
+            
