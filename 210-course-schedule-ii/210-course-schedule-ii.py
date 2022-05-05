dcea84069053065fd003f42be7bea299f148457b
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-05 07:09:05 -0400

    Time: 108 ms (79.49%), Space: 15.6 MB (65.57%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,40 +1,40 @@
+from collections import *
+
 class Solution:
-    # --- Kahn's Topological Sort Algorithm
     # Time = O(M + N)
     # Space = O(M + N)
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         N = numCourses
         M = len(prerequisites)
-        
         adj_list = [[] for _ in range(N)]
-        in_degrees = [0] * N
+        in_deg = defaultdict(int)
         
         def build_graph():
-            for (course, pre) in prerequisites:
-                adj_list[pre].append(course)
-                in_degrees[course] += 1
+            for (c, pre) in prerequisites:
+                adj_list[pre].append(c)
+                in_deg[c] += 1
             return
         
-        build_graph()
         
-        # queue <- v with in_deg = 0
-        queue = deque()
-        for (course, num_pre) in enumerate(in_degrees):
-            if num_pre == 0:
-                queue.append(course)
+        build_graph()
+        bag = deque()
+        for c in range(N):
+            if in_deg[c] == 0:
+                bag.append(c)
         
         course_list = []
-        while queue:
-            curr = queue.popleft()
+        while bag:
+            curr = bag.popleft()
             course_list.append(curr)
             
             for neighbor in adj_list[curr]:
-                in_degrees[neighbor] -= 1
-                if in_degrees[neighbor] == 0:
-                    queue.append(neighbor)
+                in_deg[neighbor] -= 1
+                if in_deg[neighbor] == 0:
+                    bag.append(neighbor)
         
-        # Check for cyclical dependency
         if len(course_list) < N:
             return []
         
         return course_list
+                
+        
