0395cdf4fb3d224ab08d876bab6ea4eeb992ec77
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-05 06:40:35 -0400

    Time: 98 ms (93.08%), Space: 17.2 MB (23.33%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,33 +1,40 @@
 class Solution:
+    # Time = O(N + M)
+    # Space = O(N + M)
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # map each course to prereq list
-        preMap = {i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+        N = numCourses
+        M = len(prerequisites)
         
-        # visitSet = all courses taken on the curr DFS path
-        visitSet = set()
-        def dfs(crs):
-            if crs in visitSet:
-                return False
-            
-            if preMap[crs] == []:
-                return True
-            
-            visitSet.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre): return False
-            
-            visitSet.remove(crs)
-            preMap[crs] = []
-            return True
+        adj_list = [[] for _ in range(N)]
+        arrival = [-1] * N
+        departure = [-1] * N
+        timestamp = [0]
         
-        for crs in range(numCourses):
-            if not dfs(crs): return False
+        def build_graph():
+            for crs, pre in prerequisites:
+                adj_list[pre].append(crs)
+            return
+        
+        def dfs(src):
+            arrival[src] = timestamp[0]
+            timestamp[0] += 1
+            
+            for neighbor in adj_list[src]:
+                if arrival[neighbor] == -1:
+                    has_cycle = dfs(neighbor)
+                    if has_cycle: return True
+                else:
+                    # Check back edge
+                    if departure[neighbor] == -1:
+                        return True # hasCycle
+            
+            departure[src] = timestamp[0]
+            timestamp[0] += 1
+            return False
+            
+        build_graph()
+        for c in range(numCourses):
+            has_cycle = dfs(c)
+            if has_cycle: return False
         
         return True
-    
-        # Not connected graph
-        # 1 -> 2 
-        # 3 -> 4
-            
