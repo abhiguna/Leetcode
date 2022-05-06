2541c1208c0728c993205436489da3ad50d3c655
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-06 10:35:22 -0400

    Time: 2847 ms (36.43%), Space: 103.8 MB (29.35%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,48 +1,57 @@
-# Approach 1: Prim's Algorithm
 class Solution:
     # Time = O(N^2logN)
     # Space = O(N^2)
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        edge_list = []
         N = len(points)
-        adj_list = [[] for _ in range(N)]
         
         def compute_dist(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+            return int(abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]))
         
         def build_graph():
             for i in range(N-1):
                 for j in range(i+1, N):
                     dist = compute_dist(points[i], points[j])
-                    adj_list[i].append((j, dist))
-                    adj_list[j].append((i, dist))
+                    # Undirected graph -> Store two copies of each edge
+                    edge_list.append([i, j, dist])
             return
-        
-        min_cost = [0]
-        visited = set()
-        pq = []
-        
-        def prims():
-            visited.add(0)
-            for (nei, weight) in adj_list[0]:
-                heappush(pq, (weight, nei))
             
-            while pq:
-                weight, curr = heappop(pq)
-                if curr in visited:
-                    continue
-                visited.add(curr)
-                min_cost[0] += weight
-                
-                # Graph connected when all nodes visited
-                if len(visited) == N:
-                    break
-                
-                for (nei, weight) in adj_list[curr]:
-                    if nei not in visited:
-                        heappush(pq, (weight, nei))
-            return
+        min_cost = [0]
+        num_components = [N]
+        parent = [i for i in range(N)]
+        size = [1]*N
         
+        def find(node):
+            # Base case: root node
+            if parent[node] == node:
+                return node
+            # Recursive case: internal node
+            root = find(parent[node])
+            parent[node] = root
+            return root
+        
+        def kruskals():
+            # Sort by edge-weight
+            edge_list.sort(key = lambda x: x[2])
+            # print(edge_list)
+            for (u, v, weight) in edge_list:
+                root_u = find(u)
+                root_v = find(v)
+                if root_u != root_v:
+                    if size[root_u] < size[root_v]:
+                        parent[root_u] = root_v
+                        size[root_v] += size[root_u]
+                    else:
+                        parent[root_v] = root_u
+                        size[root_u] += size[root_v]
+                        
+                    min_cost[0] += weight
+                    num_components[0] -= 1
+                    if num_components[0] == 1:
+                        break
+            return
+            
         build_graph()
-        prims()
+        kruskals()
         return min_cost[0]
         
