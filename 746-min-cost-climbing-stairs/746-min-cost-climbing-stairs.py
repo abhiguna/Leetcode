37f6227d7455552543d1c625a6bc35d61f3300d1
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-10 10:00:50 -0400

    Time: 91 ms (34.60%), Space: 14 MB (78.38%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -2,18 +2,18 @@ class Solution:
     # Approach: DP
     
     # Time = O(N)
-    # Space = O(N)
+    # Space = O(1)
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         N = len(cost)
-        table = [0] * N
-        # Base cases: start from step 0 or step 1
-        table[0] = cost[0]
-        table[1] = cost[1]
+        first = cost[0]
+        second = cost[1]
         
         # Compute the min cost for the remaining steps upto 1 step before the top floor
         for i in range(2, N):
-            table[i] = cost[i] + min(table[i-1], table[i-2])
+            third = cost[i] + min(first, second)
+            first = second
+            second = third
         
         # Min cost of the top floor would be either one floor below it or two floors below it
-        return min(table[N-1], table[N-2])
+        return min(first, second)
             
