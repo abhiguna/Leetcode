cf57d6620c944fb437da9daf8551b4ca592c5a81
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-10 16:01:11 -0400

    Time: 43 ms (53.87%), Space: 13.9 MB (72.76%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,19 +1,31 @@
 class Solution:
-    
-    # If nums is not cyclical
-    def helper(self, nums):
-        rob1 = 0
-        rob2 = 0
-        
-        for num in nums:
-            new_rob = max(num + rob1, rob2)
-            rob1 = rob2
-            rob2 = new_rob
-        
-        return rob2
+    # Approach: DP
     
     # Time = O(N)
     # Space = O(1)
     def rob(self, nums: List[int]) -> int:
-        # Special case: if given one element long array, max = nums[0]
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+        N = len(nums)
+        # Edge case
+        if N == 1:
+            return nums[0]
+        
+        def rob_linear(loots):
+            if len(loots) <= 0:
+                return 0
+            
+            if len(loots) == 1:
+                return loots[0]
+            
+            first = loots[0]
+            second = max(loots[0], loots[1])
+            
+            for h in range(2, len(loots)):
+                third = max(second, loots[h] + first)
+                first = second
+                second = third
+            
+            return second
+        
+        # Two cases: rob the first house or not rob it
+        return max(nums[0] + rob_linear(nums[2:N-1]), rob_linear(nums[1:N]))
+        
