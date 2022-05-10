7042e50fb48a6653d38b19695a48a502e1ec10fa
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-10 07:37:11 -0400

    Time: 70 ms (28.11%), Space: 14 MB (43.11%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -6,31 +6,35 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         M = len(obstacleGrid)
         N = len(obstacleGrid[0])
-        table = [[0 for j in range(N)] for i in range(M)]
-        table[0][0] = 0 if obstacleGrid[0][0] == 1 else 1
+        table = [0 for j in range(N)]
+        table[0] = 0 if obstacleGrid[0][0] == 1 else 1
         
         # First row
         for j in range(1, N):
             if obstacleGrid[0][j] == 1:
                 # Obstacle exists
-                table[0][j] = 0
+                table[j] = 0
             else:
-                table[0][j] = table[0][j-1]
-        
-        # First col
-        for i in range(1, M):
-            if obstacleGrid[i][0] == 1:
-                table[i][0] = 0
-            else:
-                table[i][0] = table[i-1][0]
+                table[j] = table[j-1]
         
         # Fill the remaining cells
         for i in range(1, M):
+            prev = table[:]
+            
+            if obstacleGrid[i][0] == 1:
+                table[0] = 0
+            else:
+                table[0] = prev[0]
+            
+            temp = table[0]
+            
             for j in range(1, N):
                 # Check if obstacle exists at [i][j]
                 if obstacleGrid[i][j] == 1:
-                    table[i][j] = 0
+                    temp = 0
                 else:
-                    table[i][j] = table[i-1][j] + table[i][j-1]
+                    temp = temp + prev[j]
+                
+                table[j] = temp
         
-        return table[M-1][N-1]
+        return table[N-1]
