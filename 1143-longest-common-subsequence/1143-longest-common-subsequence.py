76f968858045eb437fd78a45d3524df6cc585cd4
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-11 18:54:30 -0400

    Time: 632 ms (45.01%), Space: 22.7 MB (43.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,18 +1,21 @@
 class Solution:
+    # Approach: DP
     
     # Time = O(N*M)
-    # Space = O(N*M)
+    # Space = O(N*M), could be brought down to O(min(N, M))
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        N = len(text1)
-        M = len(text2)
+        # table[i][j] = len of the LCS if upto i chars in text1 and j chars in text2 are 
+        #    examined.
+        M = len(text1)
+        N = len(text2)
+        table = [[0 for j in range(N+1)] for i in range(M+1)]
         
-        dp = [[0 for j in range(M+1)] for i in range(N + 1)]
+        for i in range(1, M+1):
+            for j in range(1, N+1):
+                if text1[i-1] == text2[j-1]: 
+                    matches = 1
+                else: 
+                    matches = 0
+                table[i][j] = max(table[i-1][j], table[i][j-1], table[i-1][j-1] + matches)
         
-        for i in range(N-1, -1, -1):
-            for j in range(M-1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
-                else:
-                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
-                    
-        return dp[0][0]
+        return table[M][N]
