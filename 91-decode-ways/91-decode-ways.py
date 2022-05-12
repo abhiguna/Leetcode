0528b18f0b3973a6e40e1688f0f5a2af3ba11e7f
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-11 22:34:22 -0400

    Time: 54 ms (28.45%), Space: 13.9 MB (45.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,24 +1,21 @@
-from collections import *
-
 class Solution:
+    # Approach: DP
     
     # Time = O(N)
-    # Space = O(N)
+    # Space = O(N) -> could be brought down to O(1)
     def numDecodings(self, s: str) -> int:
         N = len(s)
-        dp = defaultdict(int)
-        dp[N] = 1
+        table = [0 for i in range(N+1)]
+        table[0] = 1 # One way to decode an empty string
+        table[1] = 1 if s[0] != "0" else 0
         
-        for i in range(N-1, -1, -1):
-            if s[i] == "0":
-                dp[i] = 0
-            else:
-                # Take one character
-                dp[i] = dp[i+1]
-            
-            # Take two characters if possible
-            if (i < N-1) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
-                dp[i] += dp[i+2]
-        
-        return dp[0]
+        for i in range(2, N+1):
+            # Check if the curr digit is valid
+            if s[i-1] in "123456789":
+                table[i] += table[i-1]
                 
+            # Check if the last two digits are valid
+            if s[i-2] == "1" or (s[i-2] == "2" and s[i-1] in "0123456"):
+                table[i] += table[i-2]
+        
+        return table[N]
