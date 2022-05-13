2b71b8add040283290b1792cd0faa3f32e563bd8
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-13 19:39:30 -0400

    Time: 357 ms (50.29%), Space: 14.1 MB (69.65%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,30 +1,19 @@
 class Solution:
     # Approach: DP
     
-    # Time = O(A*N*C), C = max(max_copies), which in the worst case would be A if coin denomination is a 1
-    # Space = O(A*N)
+    # Time = O(N*A)
+    # Space = O(A)
     def change(self, amount: int, coins: List[int]) -> int:
         N = len(coins)
-        A = amount
-        table = [[0 for j in range(A+1)] for i in range(N+1)]
-        table[0][0] = 1
         
-        # Base case
-        # Fill first row
-        for j in range(1, A+1):
-            table[0][j] = 0
+        # table[i] = number of different comb. of coin changes to achieve a target of amount
         
-        # Fill first col
+        table = [0 for _ in range(amount + 1)]
+        table[0] = 1
+        
         for i in range(1, N+1):
-            table[i][0] = 1
-        
-        # Fill remaining cells
-        for i in range(1, N+1):
-            for a in range(1, A+1):
-                # Exclude choice
-                table[i][a] = table[i-1][a]
-                # Include choice
+            for a in range(1, amount+1):
                 if a - coins[i-1] >= 0:
-                    table[i][a] += table[i][a-coins[i-1]]
-
-        return table[N][A]
+                    table[a] += table[a-coins[i-1]]
+        
+        return table[amount]
