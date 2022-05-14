82deead72fc17513338571eb2786e8a856201fd3
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-14 08:22:24 -0400

    Time: 24 ms (98.45%), Space: 13.9 MB (60.29%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,29 +1,17 @@
 class Solution:
+    # Approach: DP
     
+    # Time = O(N^2)
+    # Space = O(N)
     def numTrees(self, n: int) -> int:
+        # Edge case
+        if n == 1: return 1
         
-        mem = defaultdict(int)
+        table = [0 for _ in range(n+1)]
+        # Base cases
+        table[0], table[1] = 1, 1
+        for i in range(2, n+1):
+            for k in range(0, i):
+                table[i] += table[k] * table[i-k-1]
         
-        def count_bsts(n):
-            nonlocal mem
-            
-            if n in mem:
-                return mem[n]
-            
-            # Base case
-            if n == 0 or n == 1:
-                mem[n] = 1
-                return 1
-            
-            # Recursive case
-            total_bsts = 0
-            for i in range(0, n):
-                left_subtree = count_bsts(i)
-                right_subtree = count_bsts(n - 1 - i)
-                total_bsts += left_subtree * right_subtree
-            
-            mem[n] = total_bsts
-            return total_bsts
-        
-        return count_bsts(n)
-        
+        return table[n]
