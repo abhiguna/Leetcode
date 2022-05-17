5065c6b52a1b191bb0d24d9d02896d60a185262c
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-16 21:44:43 -0400

    Time: 29 ms (90.32%), Space: 14 MB (75.73%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -4,29 +4,40 @@ class MyQueue:
     def __init__(self):
         self.enqueue_stack = []
         self.dequeue_stack = []
+        self.size = 0
 
     def push(self, x: int) -> None:
         self.enqueue_stack.append(x)
+        self.size += 1
         return
 
     def pop(self) -> int:
+        # Edge case: check if queue is empty
+        if self.size == 0:
+            return None
+        
+        self.size -= 1
         # If the dequeue_stack has elements in it => pop from it
         if len(self.dequeue_stack) > 0:
             return self.dequeue_stack.pop()
         # Otherwise, transfer all the elements from enqueue_stack to dequeue_stack and then pop
-        for i in range(len(self.enqueue_stack)):
+        while len(self.enqueue_stack) != 0:
             self.dequeue_stack.append(self.enqueue_stack.pop())
         return self.dequeue_stack.pop()
 
     def peek(self) -> int:
+        if self.size == 0:
+            return None
         # If the dequeue stack still has elements in it, return the top of it
         if len(self.dequeue_stack) > 0:
             return self.dequeue_stack[-1]
-        # Otherwise, return the top of the dequeue stack
-        return self.enqueue_stack[0]
+        # Otherwise, transfer all the elements to the dequeue_stack and return the top of it
+        while len(self.enqueue_stack) != 0:
+            self.dequeue_stack.append(self.enqueue_stack.pop())
+        return self.dequeue_stack[-1]
 
     def empty(self) -> bool:
-        return (len(self.enqueue_stack) == 0) and (len(self.dequeue_stack) == 0)
+        return (self.size == 0)
         
 
 
