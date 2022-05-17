539b31c954b7a45ae54c30833ebf27cb60e3aa1c
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-17 13:35:28 -0400

    Time: 1047 ms (54.63%), Space: 75.7 MB (47.54%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,47 +1,88 @@
-from collections import defaultdict
-from sortedcontainers import SortedDict
-
+# Doubly Linked List Node
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.next = self.prev = None
-
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.max_size = capacity
-        self.cache = defaultdict()
-        self.lru, self.mru = Node(0, 0), Node(0, 0)
-        self.lru.next, self.mru.prev = self.mru, self.lru
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
         
-    # remove from lru
-    def remove(self, node):
-        prev_node, next_node = node.prev, node.next
-        prev_node.next, next_node.prev = next_node, prev_node
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.size = 0
+        # LRU elements will be at the head -> we will remove from the head
+        self.LRU = None
+        # MRU elements will be at the tail -> we will append at the tail
+        self.MRU = None
+        self.hmap = {}
+        
+    def evict_and_append(self, node):
+        del self.hmap[self.LRU.key]
+        if self.LRU == self.MRU:
+            self.LRU = self.MRU = node
+            return
+        # Evicting the least recently used element and appending the new element to the MRU side
+        self.LRU = self.LRU.next
+        self.LRU.prev = None
+        node.prev = self.MRU
+        self.MRU.next = node
+        self.MRU = self.MRU.next
+        return
     
-    # insert at mru
-    def insert(self, node):
-        prev_node, next_node = self.mru.prev, self.mru
-        prev_node.next = next_node.prev = node
-        node.prev, node.next = prev_node, next_node
-    
+    def extract_and_append(self, node):
+        node_cpy = Node(node.key, node.value)
+        # Append to the MRU side
+        self.MRU.next = node_cpy
+        node_cpy.prev = self.MRU
+        self.MRU = self.MRU.next
+        # Update hmap value for the current node
+        self.hmap[node.key] = self.MRU
+        if node == self.LRU:
+            self.LRU = self.LRU.next
+            self.LRU.prev = None
+        else:
+            # node has a prev ptr
+            if node.prev:
+                node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev 
+        return
+            
+        
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+        # Key not found in hmap
+        if key not in self.hmap:
+            return -1
+        # Key found in hashmap
+        node = self.hmap[key]
+        val = node.value 
+        # Extract this node and add it to the tail
+        self.extract_and_append(node)
+        return val
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        if len(self.cache) > self.max_size:
-            lru = self.lru.next
-            self.remove(lru)
-            del self.cache[lru.key]
-    
+        # If element already there
+        if key in self.hmap:
+            node = self.hmap[key]
+            node.value = value
+            # Append to MRU side
+            self.extract_and_append(node)
+        else:
+            new_node = Node(key, value)
+            # Check if cache full -> LRU eviction needed
+            if self.size == self.capacity:
+                self.evict_and_append(new_node)
+            else:
+                self.size += 1
+                
+                if not self.LRU:
+                    self.LRU = self.MRU = new_node
+                else:
+                    new_node.prev = self.MRU
+                    self.MRU.next = new_node
+                    self.MRU = self.MRU.next
+            self.hmap[new_node.key] = new_node
+        return
 
 
 # Your LRUCache object will be instantiated and called as such:
