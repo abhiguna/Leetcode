760f54cc31a5fa9c995a234f4914e84f0a8d45aa
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-17 15:23:38 -0400

    Time: 60 ms (20.31%), Space: 13.9 MB (39.76%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,20 +1,34 @@
-# Date: 2/16/22
-# 20m 3
+from collections import *
+
 class Solution:
-    # Pattern: Stack
-    # Time = O(n)
-    # Space = O(n)
+    # Time = O(N), N: tokens in the path
+    # Space = O(N)
     def simplifyPath(self, path: str) -> str:
-        stack = []
-        cur = ""
-        for c in path + "/":
-            if c == "/":
-                if cur == "..":
-                    if stack:
-                        stack.pop()
-                elif cur and cur != ".":
-                    stack.append(cur)
-                cur = ""
+        stack = deque()
+        split_path = path.split("/")
+        size = 0
+        for token in split_path:
+            # Check for "//" and "/."
+            if len(token) == 0 or token == ".":
+                continue
+            # If token = "..", move back one directory
+            if token == "..":
+                if size > 0:
+                    stack.pop()
+                    size -= 1
             else:
-                cur += c
-        return "/" + "/".join(stack)
+                stack.append(token)
+                size += 1
+        
+        # If in root directory -> return '/'
+        if size == 0:
+            return "/"
+        
+        # Combine the elements in the stack to form the final path
+        final_path = ""
+        for s in stack:
+            final_path = final_path + "/" + s
+            
+        return final_path
+        
+        
