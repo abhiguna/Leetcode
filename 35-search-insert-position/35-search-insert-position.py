aeb661bc1f45cdb7ba02e304e7ac7050e5ba56c5
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-20 08:31:12 -0400

    Time: 72 ms (40.75%), Space: 14.6 MB (83.10%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Time = O(logN)
+    # Space = O(1)
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        # Start contains the first element >= target
+        return start
