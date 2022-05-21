777ecb1243e067e88bc7ad5aaf62fc1d9c8dd44e
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-21 19:43:41 -0400

    Time: 60 ms (53.37%), Space: 14.2 MB (10.12%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -1,18 +1,38 @@
-# Date: 2/12/22
-# 30m 4
 class Solution:
-    # Pattern: binary search
-    # Time = O(logn)
+    # Time = O(logN)
     # Space = O(1)
     def findPeakElement(self, nums: List[int]) -> int:
+        # Edge cases
         N = len(nums)
-        low = 0
-        high = N - 1
-        while low < high:
-            mid = (low+high) // 2
-            if nums[mid] < nums[mid+1]:
-                low = mid + 1
-            else:
-                high = mid
-        return low
+        if N == 1:
+            return 0
+        # Check array of size 2
+        if nums[0] > nums[1]:
+            return 0
+        if nums[-1] > nums[-2]:
+            return N-1
         
+        
+        start = 0
+        end = N - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            # mid is a peak element
+            if mid != 0 and mid != N-1 and nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+        
+            # mid is a valley element
+            elif mid != 0 and mid != N-1 and nums[mid] < nums[mid+1] and nums[mid] < nums[mid-1]:
+                # Peak could be in left or right side, we arbitrarily choose the right side
+                start = mid + 1
+            
+            # mid is in ascending zone
+            elif mid == 0 or nums[mid] < nums[mid+1]:
+                start = mid + 1
+        
+            # mid is in descending zone
+            else:
+                end = mid - 1
+        
+        # No peak found -> this will never be hit
+        return -1
