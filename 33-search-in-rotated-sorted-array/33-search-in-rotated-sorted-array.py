56e620e76d9d017709a8985ec40f5c5eefa8fa55
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-21 21:48:19 -0400

    Time: 54 ms (55.03%), Space: 14.3 MB (57.22%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,30 +1,35 @@
 class Solution:
-    
     # Time = O(logN)
     # Space = O(1)
     def search(self, nums: List[int], target: int) -> int:
         N = len(nums)
+    
+        # Edge case: N == 1
+        if N == 1:
+            return 0 if nums[0] == target else -1
         
+        # Find the zone of the target (rotated | non-rotated)
+        target_zone = "rotated" if target > nums[-1] else "non-rotated"
         start = 0
         end = N - 1
-        
         while start <= end:
-            mid = start + (end - start) // 2
-            
+            # Check if mid and target_zone are different
+            mid = start + (end-start) // 2
             if nums[mid] == target:
                 return mid
             
-            if nums[mid] >= nums[start]:
-                if target >= nums[start] and target < nums[mid]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-                
+            if nums[mid] > nums[-1] and target_zone == "non-rotated":
+                # Move mid towards non-rotated zone
+                start = mid + 1
+            elif nums[mid] <= nums[-1] and target_zone == "rotated":
+                # Move mid towards rotated zone
+                end = mid - 1
             else:
-                if target > nums[mid] and target <= nums[end]:
+                # mid and target_zone are the same -> regular binsearch
+                if nums[mid] < target:
                     start = mid + 1
                 else:
                     end = mid - 1
-        
+            
+        # Target not found
         return -1
-                    
