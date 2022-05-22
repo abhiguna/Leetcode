138a56f829941f9e3661d39b62a3674a6c8a4711
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-21 20:49:23 -0400

    Time: 49 ms (65.76%), Space: 14.1 MB (96.32%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -2,23 +2,22 @@ class Solution:
     # Time = O(logN)
     # Space = O(1)
     def findMin(self, nums: List[int]) -> int:
-        min_val = nums[0]
-        
+        # Edge case: N == 1
         N = len(nums)
+        if N == 1:
+            return nums[0]
+        
+        # Boundary: rotated subarray 0,1,..,N-1 | non-rotated subarray 1,2,...,N 
         start = 0
         end = N - 1
-        
         while start <= end:
-            if nums[start] <= nums[end]:
-                min_val = min(min_val, nums[start])
-                break
-            
-            mid = start + (end-start) // 2
-            min_val = min(min_val, nums[mid])
-            
-            if nums[mid] >= nums[start]:
+            mid = start + (end - start) // 2
+            # If in rotated subarray
+            if nums[mid] > nums[-1]:
                 start = mid + 1
+            # Otherwise, in non-rotated subarray
             else:
                 end = mid - 1
         
-        return min_val
+        return nums[start]
+        
