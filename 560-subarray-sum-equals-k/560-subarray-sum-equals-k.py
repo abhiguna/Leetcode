b6bc689579026bfcc02b1b0f8378d6e458609b12
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-24 20:40:44 -0400

    Time: 411 ms (24.33%), Space: 18.3 MB (8.58%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -3,7 +3,7 @@ class Solution:
     # Space = O(N)
     def subarraySum(self, nums: List[int], k: int) -> int:
         # Initialize an empty hMap that stores the prefix and the count of the prefix
-        hmap = {}
+        hmap = defaultdict(int)
         
         # Base case: there is one prefix
         hmap[0] = 1
@@ -12,14 +12,11 @@ class Solution:
         
         for n in nums:
             prefix_sum += n
-            # Check if diff in hmap and update total # of subarrays accordingly
-            if prefix_sum - k in hmap:
-                total += hmap[prefix_sum - k]
             
+            # Check if diff in hmap and update total # of subarrays accordingly
+            total += hmap[prefix_sum - k]
+        
             # Update hmap
-            if prefix_sum in hmap:
-                hmap[prefix_sum] += 1
-            else:
-                hmap[prefix_sum] = 1
+            hmap[prefix_sum] += 1
         
         return total
