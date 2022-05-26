4ca07787da9e5c4ac4958fe16a16ea21529166fa
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-26 07:13:15 -0400

    Time: 1594 ms (15.06%), Space: 20.2 MB (59.51%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,27 @@
+from collections import *
+
+class Solution:
+    # Approach: Sliding window
+    
+    # Time = O(N)
+    # Space = O(1)
+    def totalFruit(self, fruits: List[int]) -> int:
+        N = len(fruits)
+        left = 0
+        max_len = 0
+        hmap = defaultdict(int) # Stores the distinct type of fruits
+        
+        for i in range(N):
+            hmap[fruits[i]] += 1
+            
+            while len(hmap) > 2:
+                hmap[fruits[left]] -= 1
+                if hmap[fruits[left]] == 0:
+                    del hmap[fruits[left]]
+                left += 1
+            
+            max_len = max(max_len, i-left+1)
+        
+        return max_len
+        
+        
