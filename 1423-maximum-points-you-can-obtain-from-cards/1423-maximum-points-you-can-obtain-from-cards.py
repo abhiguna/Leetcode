dfa9015633413fb3ed5fdbba4725f59c9c9816ac
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-26 10:20:52 -0400

    Time: 689 ms (19.86%), Space: 27 MB (88.83%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,16 +1,30 @@
-# Date: 2/3/22
-# 30m 3
 class Solution:
-    # Time = O(n)
+    # Time = O(N)
     # Space = O(1)
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         N = len(cardPoints)
-        M = N - k
-        total_sum = sum(cardPoints)
-        min_sum = sum(cardPoints[:M])
-        curr_sum = min_sum
-        for i in range(k):
-            curr_sum -= cardPoints[i]
-            curr_sum += cardPoints[i+M]
-            min_sum = min(min_sum, curr_sum)
-        return total_sum - min_sum
+        # Edge cases
+        if k == N:
+            return sum(cardPoints)
+        elif k == 1:
+            return max(cardPoints[0], cardPoints[-1])
+        
+        # Picking k cards that a maximal sum is the same as finding the minimal sum for a sliding window of
+        #.  size N - k
+        min_sum = sum(cardPoints)
+        left = 0
+        window_sum = 0
+        window_size = N - k
+        
+        for i in range(N):
+            window_sum += cardPoints[i]
+            
+            # Shrink the window to window size
+            while left <= i and (i-left+1) > window_size:
+                window_sum -= cardPoints[left]
+                left += 1
+            
+            if i-left+1 == window_size:
+                min_sum = min(min_sum, window_sum)
+        
+        return sum(cardPoints) - min_sum
