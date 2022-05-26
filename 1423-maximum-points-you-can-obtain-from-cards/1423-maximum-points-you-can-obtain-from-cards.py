98315869a991423995732a05c95cff0ae169edf2
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-26 10:27:49 -0400

    Time: 449 ms (84.52%), Space: 27.3 MB (61.73%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -11,20 +11,16 @@ class Solution:
         
         # Picking k cards that a maximal sum is the same as finding the minimal sum for a sliding window of
         #.  size N - k
-        min_sum = sum(cardPoints)
-        left = 0
+        L = N - k
         window_sum = 0
-        window_size = N - k
         
-        for i in range(N):
+        for i in range(L):
             window_sum += cardPoints[i]
+        
+        min_sum = window_sum
             
-            # Shrink the window to window size
-            while left <= i and (i-left+1) > window_size:
-                window_sum -= cardPoints[left]
-                left += 1
-            
-            if i-left+1 == window_size:
-                min_sum = min(min_sum, window_sum)
+        for i in range(L, N):
+            window_sum += cardPoints[i] - cardPoints[i-L]
+            min_sum = min(min_sum, window_sum)
         
         return sum(cardPoints) - min_sum
