7a2616d09f8e6df33ccd6a4572ae054afd7b571e
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-27 07:21:55 -0400

    Time: 1260 ms (59.13%), Space: 25 MB (86.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -2,13 +2,13 @@ class Solution:
     # Time = O(N)
     # Space = O(1)
     def maxProfit(self, prices: List[int]) -> int:
-        # D&C Approach
         N = len(prices)
-        buy_price = prices[0]
         max_profit = 0
+        curr_profit = 0
+        
         for i in range(1, N):
-            # Update max_profit
-            max_profit = max(max_profit, prices[i] - buy_price)
-            # Update buy_price
-            buy_price = min(buy_price, prices[i])
+            curr_profit = prices[i] - prices[i-1] + max(0, curr_profit)
+            max_profit = max(max_profit, curr_profit)
+        
         return max_profit
+            
