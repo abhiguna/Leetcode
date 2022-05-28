cabe9d7301c709cb701cc6ec1522e0cca88f2203
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-28 14:54:21 -0400

    Time: 59 ms (10.52%), Space: 13.8 MB (55.35%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -1,10 +1,11 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        # Find the first highest power of two
-        high_pow = 0
-        while 2**high_pow <= num:
-            high_pow += 1
+        # Find the first highest power of two > num
+        num_bits = 0
+        num_cpy = num
+        while num_cpy != 0:
+            num_cpy = num_cpy >> 1
+            num_bits += 1
         
-        bitmask = (2**high_pow) - 1
-        
+        bitmask = (1 << num_bits) - 1
         return num ^ bitmask
