314343fa9f222a343e012d49e904c85efd2ef550
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-28 10:23:23 -0400

    Time: 433 ms (12.46%), Space: 13.9 MB (8.97%) - LeetHub

--- 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.py
@@ -4,6 +4,9 @@ class Solution:
     def consecutiveNumbersSum(self, n: int) -> int:
         count = 0
         k = 1
+        # Let n = a + k
+        # Then we have to find all k's such that
+        # N = a + (a+1) + (a+2) + ... + (a+k-1)
         while (n - (k*(k-1)/2)) > 0:
             # Check if the k value is legal such that n is reachable
             if (n - (k*(k-1))/2) % k == 0:
