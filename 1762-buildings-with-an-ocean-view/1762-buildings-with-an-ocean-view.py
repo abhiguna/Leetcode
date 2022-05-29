645f1abb021c3a426893d0f19c284ca07d456993
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-29 13:30:22 -0400

    Time: 940 ms (43.57%), Space: 32 MB (7.14%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,17 +1,19 @@
 class Solution:
-    # Approach 2: Using pointers
-    
     # Time = O(N)
-    # Space = O(1)
+    # Space = O(N)
     def findBuildings(self, heights: List[int]) -> List[int]:
+        stack = deque()
         N = len(heights)
-        curr_max = heights[N-1]
-        res = [N-1]
-        for i in range(N-2, -1, -1):
-            if heights[i] > curr_max:
-                res.append(i)
-                curr_max = heights[i]
-        # Reverse the result
-        res.reverse()
-        return res
         
+        for i in range(N):
+            # Pop out all the elements that have a height <= current height
+            while stack and stack[-1][0] <= heights[i]:
+                stack.pop()
+            stack.append((heights[i], i))
+        
+        res = []
+        for (height, idx) in stack:
+            res.append(idx)
+        
+        return res
+            
