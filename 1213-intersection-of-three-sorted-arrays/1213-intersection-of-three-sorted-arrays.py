a3465aff70874220fe626f9a52666019c1ff0143
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 15:50:25 -0400

    Time: 109 ms (62.58%), Space: 14.2 MB (44.10%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.py
@@ -1,26 +1,22 @@
 class Solution:
-    
-    def intersection(self, A, B):
-        M = len(A)
-        N = len(B)
-        i = 0
-        j = 0
-        res = []
-        while i < M and j < N:
-            if A[i] < B[j]:
-                i += 1
-            elif B[j] < A[i]:
-                j += 1
-            else:
-                # intersection
-                res.append(A[i])
-                i += 1
-                j += 1
-        return res
-    
-    # Time = O(N + M + O): N, M, O: len(arr1), len(arr2), len(arr3)
+    # Time = O(M + N + K), M: len(arr1), N: len(arr2), K: len(arr3)
     # Space = O(1)
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        first = self.intersection(arr1, arr2)
-        res = self.intersection(first, arr3)
+        
+        def intersect_two(nums1, nums2):
+            res = []
+            i, j = 0, 0
+            while i < len(nums1) and j < len(nums2):
+                if nums1[i] == nums2[j]:
+                    res.append(nums1[i])
+                    i += 1
+                    j += 1
+                elif nums1[i] < nums2[j]:
+                    i += 1
+                else:
+                    j += 1
+            return res
+            
+        intersection = intersect_two(arr1, arr2)
+        res = intersect_two(intersection, arr3)
         return res
