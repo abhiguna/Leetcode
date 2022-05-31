d587533bc6e93e686dbcab0f2f0abae3ede37021
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 11:33:44 -0400

    Time: 1070 ms (28.46%), Space: 25.3 MB (5.90%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,27 +1,44 @@
 class Solution:
-    # Approach: Quick sort -> in-place
-    # Time = O(NlogN) amortized
-    # Space = O(1) excluding implicit stack space 
+    # Approach 2: Merge Sort -> stable, NOT in-place
+    
+    # Time = O(NlogN) -> T(N) = 2T(N/2) + cn
+    # Space = O(N)
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quicksort(start, end):
+        def merge(start, mid, end):
+            i, j = start, mid+1
+            aux = []
+            while i <= mid and j <= end:
+                if nums[i] <= nums[j]:
+                    aux.append(nums[i])
+                    i += 1
+                else:
+                    aux.append(nums[j])
+                    j += 1
+            
+            while i <= mid:
+                aux.append(nums[i])
+                i += 1
+            
+            while j <= end:
+                aux.append(nums[j])
+                j += 1
+            
+            nums[start:end+1] = aux
+            return
+        
+        def merge_sort(start, end):
             # Base case
             if start >= end:
                 return
             
-            pivot_idx = random.randint(start, end)
-            (nums[start], nums[pivot_idx]) = (nums[pivot_idx], nums[start])
-            pivot = nums[start]
-            
-            left = start
-            for right in range(start+1, end+1):
-                if nums[right] <= pivot:
-                    left += 1
-                    (nums[left], nums[right]) = (nums[right], nums[left])
-            
-            (nums[start], nums[left]) = (nums[left], nums[start])
-            quicksort(start, left-1)
-            quicksort(left+1, end)
+            mid = start + (end-start) // 2
+            merge_sort(start, mid)
+            merge_sort(mid+1, end)
+            merge(start, mid, end)
             return
-            
-        quicksort(0, len(nums) - 1)
+        
+        
+        merge_sort(0, len(nums)-1)
         return nums
+        
+        
