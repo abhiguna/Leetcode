a3c18e69c7ee08f4c9390ec77d90baafceffe6a2
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 17:43:18 -0400

    Time: 116 ms (73.39%), Space: 19 MB (22.37%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,39 @@
-from collections import Counter
-import heapq
-
 class Solution:
+    # Time = O(N) amortized time
+    # Space = O(1)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        heap = []
-        heapq.heapify(heap)
-        for num, count in freq.items():
-            heapq.heappush(heap, (count, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        result = []
-        while heap:
-            freq_num = heapq.heappop(heap)
-            result.append(freq_num[1])
-        return result
+        hmap = Counter(nums)
+        nums = [(num, freq) for (num, freq) in hmap.items()]
+        
+        def helper(start, end):
+            if start >= end:
+                return
+            
+            pivot_idx = random.randint(start, end)
+            (nums[pivot_idx], nums[start]) = (nums[start], nums[pivot_idx])
+            pivot = nums[start][1]
+            # Lomuto's partitioning 
+            left = start
+            for right in range(start+1, end+1):
+                if nums[right][1] <= pivot:
+                    left += 1
+                    (nums[left], nums[right]) = (nums[right], nums[left])
+            
+            (nums[start], nums[left]) = (nums[left], nums[start])
+            
+            if left == len(nums) - k:
+                return
+            elif left < len(nums) - k:
+                return helper(left+1, end)
+            else:
+                return helper(start, left-1)
+            
+        helper(0, len(nums)-1)
+        # Update nums
+        for i in range(len(nums)-k, len(nums)):
+            nums[i] = nums[i][0]
+        
+        return nums[len(nums)-k:]
+        
+        
+        
