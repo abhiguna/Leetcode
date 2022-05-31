39e6bea5282e8d714f4faba6b019d825fc2eb9ae
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 11:57:39 -0400

    Time: 160 ms (8.18%), Space: 17.2 MB (79.64%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -2,13 +2,17 @@ class Solution:
     # Time = O(NlogN)
     # Space = O(N)
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key = lambda x: x[0])
         N = len(intervals)
+        # Sort by start time
+        intervals.sort(key=lambda x: x[0])
         
-        for i in range(N-1):
-            next_start = intervals[i+1][0]
-            # New interval
-            if intervals[i][1] > next_start:
+        for i in range(N):
+            if i == N - 1:
+                next_start = math.inf
+            else:
+                next_start = intervals[i+1][0]
+            
+            if next_start < intervals[i][1]:
                 return False
         
         return True
