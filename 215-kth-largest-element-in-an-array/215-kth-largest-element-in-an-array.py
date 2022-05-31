0b322e6279502798a6950ce1288993591bfb1946
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 16:07:39 -0400

    Time: 70 ms (85.01%), Space: 15.4 MB (14.95%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,13 +1,31 @@
-import heapq
-
 class Solution:
-    # Time = O(nlogk)
-    # Space = O(k)
+    # Time = O(N), N: len(nums)
+    # Space = O(1) explicit aux. space
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        heapq.heapify(heap)
-        for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return heapq.heappop(heap)
+        def helper(start, end):
+            pivot_idx = random.randint(start, end)
+            (nums[start], nums[pivot_idx]) = (nums[pivot_idx], nums[start])
+            pivot = nums[start]
+            
+            # Partition
+            left = start
+            for right in range(start+1, end+1):
+                if nums[right] <= pivot:
+                    left += 1
+                    (nums[left], nums[right]) = (nums[right], nums[left])
+            
+            # Swap pivot with the left ptr
+            (nums[start], nums[left]) = (nums[left], nums[start])
+            
+            # 3 cases
+            if left == len(nums) - k:
+                return nums[left]
+            elif left < len(nums) - k:
+                return helper(left+1, end)
+            else:
+                return helper(start, left-1)
+            
+            
+        
+        kth_largest = helper(0, len(nums) - 1)
+        return kth_largest
