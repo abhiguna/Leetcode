719fbb1fa8705a2dda70cfb2a104bc7f8a68ac30
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 17:32:30 -0400

    Time: 1034 ms (51.67%), Space: 20.3 MB (80.27%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,46 +1,34 @@
-import math
-import random
-
 class Solution:
-    def get_distance(self, point):
-        return math.sqrt((point[0] ** 2) + (point[1] ** 2))
-        
-    def partition(self, points, start, end):
-        pivot_idx = random.randint(start, end)
-        # Swap
-        points[start], points[pivot_idx] = points[pivot_idx], points[start]
-        smaller = start
-        for bigger in range(start + 1, end + 1):
-            if self.get_distance(points[bigger]) <= self.get_distance(points[start]):
-                smaller += 1
-                # Swap
-                points[smaller], points[bigger] = points[bigger], points[smaller]
-        # Swap with pivot_idx
-        points[start], points[smaller] = points[smaller], points[start]
-        return smaller
-        
-    def helper(self, points, start, end, target_idx):
-        # Base case
-        if start == end:
-            return
-        
-        pivot_idx = self.partition(points, start, end)
-        if pivot_idx == target_idx:
-            return
-        elif pivot_idx < target_idx:
-            self.helper(points, pivot_idx + 1, end, target_idx)
-        else:
-            self.helper(points, start, pivot_idx - 1, target_idx)
-    
     # Time = O(N)
     # Space = O(1)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        N = len(points)
-        # Edge case
-        if N == 1:
-            return points
+        def get_distance(point):
+            return math.sqrt((point[0]**2) + (point[1]**2))
         
-        self.helper(points, 0, N - 1, k - 1)
+        def helper(start, end):
+            if start >= end:
+                return 
+            
+            pivot_idx = random.randint(start, end)
+            (points[start], points[pivot_idx]) = (points[pivot_idx], points[start])
+            pivot = get_distance(points[start])
+            # Lomuto's partitioning 
+            left = start
+            for right in range(start+1, end+1):
+                if get_distance(points[right]) <= pivot:
+                    left += 1
+                    (points[left], points[right]) = (points[right], points[left])
+            
+            (points[start], points[left]) = (points[left], points[start])
+            
+            if left == k:
+                return
+            elif left < k:
+                return helper(left + 1, end)
+            else:
+                return helper(start, left - 1)
+            
+            
+        
+        helper(0, len(points) - 1)
         return points[:k]
-        
-        
