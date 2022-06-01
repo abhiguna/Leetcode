161c29112465f16ca78f933fbfc2f758a181bbf9
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 11:26:47 -0400

    Time: 76 ms (14.03%), Space: 14.1 MB (58.27%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,17 +1,24 @@
 class Solution:
+    # Time = O(N*N!)
+    # Space = O(N)
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        # Base case
-        if len(nums) == 1:
-            return [nums[:]]
+        N = len(nums)
+        res = []
         
-        for i in range(len(nums)):
-            first_num = nums.pop(0)
-            perms = self.permute(nums)
-            for perm in perms:
-                perm.append(first_num)
-                result.append(perm)
-            nums.append(first_num)
-        return result
-    
-            
+        def helper(anchor_idx):
+            # Base case
+            if anchor_idx == N:
+                res.append(nums[:])
+                return
+            else:
+                for i in range(anchor_idx, N):
+                    # Swap with anchor idx
+                    (nums[i], nums[anchor_idx]) = (nums[anchor_idx], nums[i])
+                    helper(anchor_idx + 1)
+                    # Unswap
+                    (nums[i], nums[anchor_idx]) = (nums[anchor_idx], nums[i])
+                return
+        
+        # Root manager
+        helper(0)
+        return res
