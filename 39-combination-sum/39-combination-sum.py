88c0eb50b40b21674341d6fcdc02ac99e9a2a5b0
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 14:35:31 -0400

    Time: 100 ms (62.74%), Space: 14 MB (25.81%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,37 @@
+class Solution:
+    # Time = O(N*2^N)
+    # Space = O(N)
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        N = len(candidates)
+        res = []
+        candidates.sort()
+        
+        def helper(idx, target_left, slate):
+            # Backtracking case
+            if (target_left < 0) or (idx == N and target_left != 0):
+                return
+            
+            # Base case
+            if target_left == 0:
+                res.append(slate[:])
+                return
+            
+            # General case
+            # Exclude
+            helper(idx+1, target_left, slate)
+            
+            # Include
+            count = target_left // candidates[idx]
+            for i in range(1, count+1):
+                slate += [candidates[idx]] * i
+                target_left -= (candidates[idx]) * i
+                helper(idx+1, target_left, slate)
+                for j in range(i):
+                    slate.pop()
+                    target_left += candidates[idx]
+    
+            return
+            
+        
+        helper(0, target, [])
+        return res
