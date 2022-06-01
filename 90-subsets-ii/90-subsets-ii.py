e779ff8c2a634a8aefb45447069ae7b524b9ee71
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 11:02:06 -0400

    Time: 63 ms (29.05%), Space: 14.2 MB (49.24%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,38 +1,30 @@
 class Solution:
-    # Time = O(N * 2^N)
+    # Time = O(N*2^N)
     # Space = O(N)
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         N = len(nums)
+        # Sort the current input
+        nums.sort()
         res = []
         
-        def dfs(idx, slate):
-            # Base case
+        def helper(idx, slate):
             if idx == N:
                 res.append(slate[:])
-                return
-            
-            # Recursive case
-            count = 0
-            for i in range(idx, N):
-                if nums[i] != nums[idx]:
-                    break
-                count += 1
-            
-            # Exclude case
-            dfs(idx + count, slate)
-            
-            # Include case
-            for i in range(count):
+            else:
+                # Find the number of times the dup element occurs
+                dup = idx
+                while dup < N-1 and nums[dup] == nums[dup+1]:
+                    dup += 1
+                
+                # Exclude case
+                helper(dup+1, slate)
+                
+                # Include case
                 slate.append(nums[idx])
-                dfs(idx + count, slate)
-            
-            # Pop all the elements from the slate
-            while count > 0:
+                helper(idx+1, slate)
                 slate.pop()
-                count -= 1
-            
-            return
-        
-        nums.sort()
-        dfs(0, [])
+                return
+                
+        # Root manager
+        helper(0, [])
         return res
