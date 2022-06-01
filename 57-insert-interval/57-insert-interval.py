6232d163d215332f1dd6c2829c54441527ac7da1
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 23:24:06 -0400

    Time: 184 ms (5.04%), Space: 17.3 MB (92.51%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -3,30 +3,23 @@ class Solution:
     # Space = O(1)
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         N = len(intervals)
-        # Edge case: N == 0
-        if N == 0:
-            return [newInterval]
-        
         res = []
+        break_idx = N
         
-        i = 0
-        # Skip all the events that end before the newInterval
-        while i < N and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
-        
-        # Overlapping intervals
-        while i < N and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
+        # Add all the intervals before the newInterval
+        for i in range(N):
+            if intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+            else:
+                break_idx = i
+                break
         
         res.append(newInterval)
         
-        # Add the remaining elements
-        while i < N:
-            res.append(intervals[i])
-            i += 1
+        for i in range(break_idx, N):
+            if intervals[i][0] <= res[-1][1]:
+                res[-1] = [min(res[-1][0], intervals[i][0]), max(res[-1][1], intervals[i][1])]
+            else:
+                res.append(intervals[i])
         
         return res
-        
