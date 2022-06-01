e368410689d7d076290195a7cac0da5fe51192a2
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 18:59:40 -0400

    Time: 33 ms (79.13%), Space: 13.9 MB (16.42%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.py
@@ -1,23 +1,17 @@
 class Solution:
-    
-    """
-        "0" n = 1
-        "01" n = 2
-        "0110" n = 3
-        "01101001" n = 4, k = 5
-        
-    """
+    # Time = O(N)
+    # Space = O(N)
     def kthGrammar(self, n: int, k: int) -> int:
-        # Base Case
+        # Base case
         if n == 1 and k == 1:
             return 0
         
         mid = (2**(n-1)) // 2
-        
         if k <= mid:
             return self.kthGrammar(n-1, k)
         else:
             ret_val = self.kthGrammar(n-1, k-mid)
-            if ret_val == 1:
+            if ret_val == 0:
+                return 1
+            else:
                 return 0
-            return 1
