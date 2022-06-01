0f124ca7c04799ca9d9bfa838a9e3800e7afe0dd
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 23:18:03 -0400

    Time: 74 ms (99.08%), Space: 17.3 MB (92.51%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -3,26 +3,30 @@ class Solution:
     # Space = O(1)
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         N = len(intervals)
-        result = []
-        i = 0
+        # Edge case: N == 0
+        if N == 0:
+            return [newInterval]
         
-        # Intervals before -> no overlap
+        res = []
+        
+        i = 0
+        # Skip all the events that end before the newInterval
         while i < N and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
+            res.append(intervals[i])
             i += 1
         
-        
-        # Overlap
+        # Overlapping intervals
         while i < N and intervals[i][0] <= newInterval[1]:
             newInterval[0] = min(newInterval[0], intervals[i][0])
             newInterval[1] = max(newInterval[1], intervals[i][1])
             i += 1
         
-        result.append(newInterval)
+        res.append(newInterval)
         
-        # Intervals after -> no overlap
+        # Add the remaining elements
         while i < N:
-            result.append(intervals[i])
+            res.append(intervals[i])
             i += 1
         
-        return result
+        return res
+        
