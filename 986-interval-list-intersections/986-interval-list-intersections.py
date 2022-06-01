972b6a1820f23500fe74e823c57eccd212653028
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 00:21:53 -0400

    Time: 154 ms (89.42%), Space: 14.9 MB (49.76%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,26 +1,27 @@
 class Solution:
-    
-    # Time = O(N)
+    # Time = O(M+N)
     # Space = O(1)
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        M = len(firstList)
-        N = len(secondList)
-        i = 0
-        j = 0
-        intersections = []
+        M, N = len(firstList), len(secondList)
+        # Edge case: M == 0 or N == 0
+        if M == 0 or N == 0:
+            return []
         
+        intersections = []
+        i, j = 0, 0
         while i < M and j < N:
-            i_overlaps_j = firstList[i][0] >= secondList[j][0] and firstList[i][0] <= secondList[j][1]
-            j_overlaps_i = secondList[j][0] >= firstList[i][0] and secondList[j][0] <= firstList[i][1]
-            
-            # Check overlap
-            if i_overlaps_j or j_overlaps_i:
-                intersections.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
-            
-            # Look at next interval
-            if firstList[i][1] < secondList[j][1]:
+            # No Overlap
+            if firstList[i][1] < secondList[j][0]:
                 i += 1
-            else:
+            elif secondList[j][1] < firstList[i][0]:
                 j += 1
+            # Overlap
+            else:
+                intersections.append([max(firstList[i][0], secondList[j][0]), \
+                                     min(firstList[i][1], secondList[j][1])])
+                if firstList[i][1] < secondList[j][1]:
+                    i += 1
+                else:
+                    j += 1
         
         return intersections
