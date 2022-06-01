25f7756a6ac3f3b58817320bbcd7ed8bd699519b
Abhishek Gunasekar <agunase@purdue.edu>
2022-05-31 20:33:04 -0400

    Time: 138 ms (21.99%), Space: 17.5 MB (82.81%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -18,11 +18,11 @@ class Solution:
                 next_start = intervals[i+1][0]
             
             # Process the current interval
-            heappush(min_heap, (intervals[i][1], id(intervals[i][1]), intervals[i]))
+            heappush(min_heap, intervals[i][1])
             min_rooms = max(min_rooms, len(min_heap))
             
             # End the current interval -> remove all intervals that end before the next event
-            while min_heap and min_heap[0][0] <= next_start:
+            while min_heap and min_heap[0] <= next_start:
                 heappop(min_heap)
         
         return min_rooms
