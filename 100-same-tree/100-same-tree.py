39e096d504421b9029aa7949a63a97d30767cd11
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 07:33:23 -0400

    Time: 38 ms (65.32%), Space: 13.9 MB (29.17%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -4,46 +4,45 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution:
-    
-    # Time = O(N)
-    # Space = O(N)
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Edge case: empty tree
         if not p and not q:
             return True
-        elif p and not q:
-            return False
-        elif not p and q:
+        elif (p and not q) or (not p and q):
             return False
         
-        queue = deque([(p, q)])
+        queue_p = deque([p])
+        queue_q = deque([q])
         
-        while queue:
-            num_nodes = len(queue)
+        while queue_p and queue_q:
+            num_nodes_p, num_nodes_q = len(queue_p), len(queue_q)
             
-            for _ in range(num_nodes):
-                node_p, node_q = queue.popleft()
+            # Structural check
+            if num_nodes_p != num_nodes_q:
+                return False
+            
+            for i in range(min(num_nodes_p, num_nodes_q)):
+                node_p = queue_p.popleft()
+                node_q = queue_q.popleft()
                 
+                # Check if values don't match
                 if node_p.val != node_q.val:
                     return False
                 
-                # Both have left child
-                if node_p.left and node_q.left:
-                    queue.append((node_p.left, node_q.left))
-                # One has left child while one doesn;t
-                elif node_p.left or node_q.left:
+                # Check if structurally they don't match
+                if (node_p.left and not node_q.left) or (not node_p.left and node_q.left):
+                    return False
+                if (node_p.right and not node_q.right) or (not node_p.right and node_q.right):
                     return False
                 
-                if node_p.right and node_q.right:
-                    queue.append((node_p.right, node_q.right))
-                elif node_p.right or node_q.right:
-                    return False
+                if node_p.left:
+                    queue_p.append(node_p.left)
+                    queue_q.append(node_q.left)
+                
+                if node_p.right:
+                    queue_p.append(node_p.right)
+                    queue_q.append(node_q.right)
         
         return True
-                
-                
-                
-                
-            
+                    
