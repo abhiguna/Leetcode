b7cdf7e89ba26032082318adba88c22c1139f12c
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 09:32:26 -0400

    Time: 92 ms (5.27%), Space: 15 MB (92.89%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -5,31 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    # Time = O(N)
+    # Time = O(N), N: # of nodes in the tree
     # Space = O(N)
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # Edge case: empty tree
         if not root:
             return False
         
-        res = [False]
-        def dfs(node, target):
-            nonlocal res
-            target -= node.val
-            
+        found_sum = [False]
+        
+        def dfs(node, path_sum):
             # Base case: leaf node
             if not node.left and not node.right:
-                if target == 0:
-                    res[0] = True
+                path_sum += node.val 
+                if path_sum == targetSum:
+                    found_sum[0] = True
+                path_sum -= node.val
+                return
             
+            # Recursive case: internal node
+            path_sum += node.val
             if node.left:
-                dfs(node.left, target)
-            
+                dfs(node.left, path_sum)
             if node.right:
-                dfs(node.right, target)
-            
+                dfs(node.right, path_sum)
+            path_sum -= node.val 
             return
-    
-        dfs(root, targetSum)
-        return res[0]
+            
+        
+        dfs(root, 0)
+        return found_sum[0]
             
