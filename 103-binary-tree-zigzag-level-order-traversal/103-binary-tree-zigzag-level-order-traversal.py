a6d8f2b73de93ada3bcfa3612239f8750441b683
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 21:23:26 -0400

    Time: 23 ms (99.66%), Space: 14.1 MB (57.33%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -5,33 +5,36 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # Time = O(N), N: # of nodes in the tree
+    # Space = O(N)
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # Edge case: empty tree
         if not root:
             return []
         
         res = []
         queue = deque([root])
-        is_lr = True
+        l_to_r = True
         
         while queue:
             num_nodes = len(queue)
             curr_level = deque()
             
             for _ in range(num_nodes):
-                curr_node = queue.popleft()
+                node = queue.popleft()
+                if l_to_r:
+                    curr_level.append(node.val)
+                else:
+                    curr_level.appendleft(node.val)
                 
-                curr_level.append(curr_node.val)
+                if node.left:
+                    queue.append(node.left)
                 
-                if curr_node.left:
-                    queue.append(curr_node.left)
-                
-                if curr_node.right:
-                    queue.append(curr_node.right)
+                if node.right:
+                    queue.append(node.right)
             
-            if not is_lr:
-                curr_level.reverse()
-            
-            is_lr = not is_lr
-            res.append(curr_level)
+            l_to_r = not l_to_r
+            res.append(list(curr_level))
         
         return res
+        
