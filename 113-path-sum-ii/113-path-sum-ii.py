188a4d6468ce3ddd38ec95e3cb5e6a204a243e2b
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 09:41:37 -0400

    Time: 48 ms (84.10%), Space: 15.5 MB (88.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -5,34 +5,37 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Time = O(N)
+    # Time = O(N), N: # of nodes in the tree
     # Space = O(N)
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        # Edge case: empty tree
         if not root:
             return []
         
         res = []
         
-        def dfs(node, slate, target):
-            nonlocal res
-            slate.append(node.val)
-            target -= node.val
-            
+        def dfs(node, slate, path_sum):
             # Base case: leaf node
             if not node.left and not node.right:
-                if target == 0:
+                path_sum += node.val
+                slate.append(node.val)
+                if path_sum == targetSum:
                     res.append(slate[:])
+                path_sum -= node.val
+                slate.pop()
+                return
             
-            # Recursive case: internal node
+            # Internal node: internal node
+            path_sum += node.val
+            slate.append(node.val)
+            
             if node.left:
-                dfs(node.left, slate, target)
-            
+                dfs(node.left, slate, path_sum)
             if node.right:
-                dfs(node.right, slate, target)
-                
-            slate.pop()
-            return
-        
-        dfs(root, [], targetSum)
-        return res
+                dfs(node.right, slate, path_sum)
             
+            path_sum -= node.val
+            slate.pop()
+        
+        dfs(root, [], 0)
+        return res
