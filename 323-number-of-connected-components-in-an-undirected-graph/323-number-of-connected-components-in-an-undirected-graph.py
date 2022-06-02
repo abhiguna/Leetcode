9151e9ca06370172246e2013427d0a7c84543899
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 19:03:45 -0400

    Time: 148 ms (43.03%), Space: 17.2 MB (19.57%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,37 +1,29 @@
 class Solution:
-    # Time = O(N + M), N: num of vertices, M: num of edges
-    # Space = O(N + M)
+    # Time = O(M+N)
+    # Space = O(M+N)
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        visited = [-1] * n # -1 = unvisited
         adj_list = [[] for i in range(n)]
         
-        # edge list -> adj list
-        def build_graph(n, edges):
-            for (src, dest) in edges:
-                adj_list[src].append(dest)
-                # Add another copy since undirected graph
-                adj_list[dest].append(src)
+        def build_graph():
+            for (u,v) in edges:
+                adj_list[u].append(v)
+                adj_list[v].append(u)
+            return
+            
+        def dfs(src):
+            visited[src] = 1
+            
+            for nei in adj_list[src]:
+                if visited[nei] == -1:
+                    dfs(nei)
             return
         
-        def dfs(source):
-            visited[source] = 1
-            
-            for n in adj_list[source]:
-                if visited[n] == -1:
-                    dfs(n)
-            return
-        
-        build_graph(n, edges)
-        num_components = 0
-        
-        for src in range(n):
-            if visited[src] == -1:
-                # Not visited
-                num_components += 1
-                dfs(src)
-        
-        return num_components
-        
-        
-            
+        build_graph()
+        visited = [-1] * n
+        components = 0
+        for i in range(n):
+            if visited[i] == -1:
+                components += 1
+                dfs(i)
+        return components
         
