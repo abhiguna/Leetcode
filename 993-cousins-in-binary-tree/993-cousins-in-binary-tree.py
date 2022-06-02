3e918fc7c05d65da8fc3f9c3ba6f554e45d9568e
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-01 23:00:19 -0400

    Time: 60 ms (18.12%), Space: 14 MB (45.78%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.py
@@ -4,43 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque
-
 class Solution:
-    
-    # Time = O(N)
-    # Space = O(N)
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        parent_x = None
-        parent_y = None
+        # Edge case: empty tree
+        if not root:
+            return False
         
-        curr_depth = -1
+        curr_level = 0
+        parent_x, level_x = -1, -1
+        parent_y, level_y = -1, -1
+        
         queue = deque([root])
         
         while queue:
             num_nodes = len(queue)
-            curr_depth += 1
             
             for _ in range(num_nodes):
-                curr_node = queue.popleft()
+                node = queue.popleft()
+                if node.val == x:
+                    level_x = curr_level
+                if node.val == y:
+                    level_y = curr_level
                 
-                if curr_node.left:
-                    queue.append(curr_node.left)
-                    if curr_node.left.val == x:
-                        parent_x = curr_node.val
-                    elif curr_node.left.val == y:
-                        parent_y = curr_node.val
+                if node.left:
+                    if node.left.val == x:
+                        parent_x = node
+                    elif node.left.val == y:
+                        parent_y = node
+                    queue.append(node.left)
                 
-                if curr_node.right:
-                    queue.append(curr_node.right)
-                    if curr_node.right.val == x:
-                        parent_x = curr_node.val
-                    elif curr_node.right.val == y:
-                        parent_y = curr_node.val
-                
-                # If either parent is found in the curr level check bool condition
-            if parent_x or parent_y:
-                return (parent_x and parent_y) and (parent_x != parent_y)
+                if node.right:
+                    if node.right.val == x:
+                        parent_x = node
+                    elif node.right.val == y:
+                        parent_y = node
+                    queue.append(node.right)
+            
+            curr_level += 1
         
-        return False
+        return (level_x == level_y) and (parent_x != parent_y)
+                
