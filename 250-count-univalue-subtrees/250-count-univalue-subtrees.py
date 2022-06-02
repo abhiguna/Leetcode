8e57a3861a13e7855a459b345d947a0eae248a90
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 11:49:40 -0400

    Time: 54 ms (38.51%), Space: 14.1 MB (61.26%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.py
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.py
@@ -5,41 +5,39 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    # Time = O(N)
+    # Time = O(N), N: # of nodes in the tree
     # Space = O(N)
     def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        # Edge case: empty tree
         if not root:
             return 0
+        
+        total_count = [0]
         
         def dfs(node):
             # Base case: leaf node
             if not node.left and not node.right:
-                return (1, True)
+                total_count[0] += 1
+                return True
             
             # Recursive case: internal node
-            is_curr_unival = True
-            num_unival = 0
+            unival_count = 1
             
             if node.left:
-                (num_left_unival, is_left_unival) = dfs(node.left)
-                
-                if not is_left_unival or node.val != node.left.val:
-                    is_curr_unival = False
-                num_unival += num_left_unival
+                is_univalue = dfs(node.left)
+                if not is_univalue or node.val != node.left.val:
+                    unival_count = 0
             
             if node.right:
-                (num_right_unival, is_right_unival) = dfs(node.right)
-                
-                if not is_right_unival or node.val != node.right.val:
-                    is_curr_unival = False
-                
-                num_unival += num_right_unival
+                is_univalue = dfs(node.right)
+                if not is_univalue or node.val != node.right.val:
+                    unival_count = 0
             
-            if is_curr_unival:
-                num_unival += 1
+            if unival_count == 0:
+                return False
             
-            return (num_unival, is_curr_unival)
-        
-        (total_unival, is_root_unival) = dfs(root)
-        return total_unival
+            total_count[0] += 1
+            return True
+                    
+        is_univalue = dfs(root)
+        return total_count[0]
