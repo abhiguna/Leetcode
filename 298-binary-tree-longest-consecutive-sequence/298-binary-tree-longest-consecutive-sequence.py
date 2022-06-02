6799b5decfa64fa7dd850c47319d826226750170
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 09:51:17 -0400

    Time: 176 ms (24.88%), Space: 21.5 MB (41.50%) - LeetHub

--- 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.py
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.py
@@ -4,35 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-import math
-
 class Solution:
-    # Time = O(N)
+    # Time = O(N), N: # of nodes in the tree
     # Space = O(N)
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+        # Edge case: empty tree
+        if not root:
+            return 0
         
-        max_len = 0
+        max_len = [0]
         
         def dfs(node, prev_val, curr_len):
-            nonlocal max_len
+            # Base case: leaf node
+            if not node.left and not node.right:
+                if node.val == prev_val + 1:
+                    curr_len += 1
+                else:
+                    curr_len = 1
+                max_len[0] = max(max_len[0], curr_len)
+                return
             
+            # Recursive case: internal node
             if node.val == prev_val + 1:
                 curr_len += 1
             else:
                 curr_len = 1
-            
-            max_len = max(max_len, curr_len)
+            max_len[0] = max(max_len[0], curr_len)
             
             if node.left:
                 dfs(node.left, node.val, curr_len)
             
             if node.right:
                 dfs(node.right, node.val, curr_len)
-                
-            max_len = max(max_len, curr_len)
+            
             return
+            
         
         
-        dfs(root, root.val, 0)
-        return max_len
+        dfs(root, root.val-1, 0)
+        return max_len[0]
