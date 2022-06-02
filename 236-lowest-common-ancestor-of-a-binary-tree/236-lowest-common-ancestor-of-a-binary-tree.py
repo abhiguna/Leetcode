1fab572ae0a08c8c3d2f435c687406e66746c653
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-02 12:17:44 -0400

    Time: 107 ms (39.04%), Space: 26.5 MB (15.82%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,17 +6,17 @@
 #         self.right = None
 
 class Solution:
-    
-    # Time = O(N)
+    # Time = O(N), N: # of nodes in the tree
     # Space = O(N)
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        LCA = None
+        # Edge case: empty tree
+        if not root:
+            return None
+        
+        LCA = [None]
         
         def dfs(node):
-            nonlocal LCA
-            
-            p_found = False
-            q_found = False
+            p_found, q_found = False, False
             
             if node.val == p.val:
                 p_found = True
@@ -24,25 +24,25 @@ class Solution:
             if node.val == q.val:
                 q_found = True
             
-            # Base Case: leaf node
+            # Base case: leaf node
             if not node.left and not node.right:
                 return (p_found, q_found)
             
             # Recursive case: internal node
             if node.left:
-                (p_found_left, q_found_left) = dfs(node.left)
-                p_found = p_found or p_found_left
-                q_found = q_found or q_found_left
+                (p_in_left, q_in_left) = dfs(node.left)
+                p_found = p_found or p_in_left
+                q_found = q_found or q_in_left
             
             if node.right:
-                (p_found_right, q_found_right) = dfs(node.right)
-                p_found = p_found or p_found_right
-                q_found = q_found or q_found_right
+                (p_in_right, q_in_right) = dfs(node.right)
+                p_found = p_found or p_in_right
+                q_found = q_found or q_in_right
             
-            if not LCA and p_found and q_found:
-                LCA = node
+            if p_found and q_found and LCA[0] == None:
+                LCA[0] = node
             
             return (p_found, q_found)
-    
-        dfs(root)
-        return LCA
+        
+        (p_found, q_found) = dfs(root)
+        return LCA[0]
