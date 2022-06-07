805016345eaeaf7493904e4c943e5aa28e826aa7
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 08:56:15 -0400

    Time: 1262 ms (27.52%), Space: 20.3 MB (59.32%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -3,32 +3,35 @@ class Solution:
     # Space = O(1)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def get_distance(point):
-            return math.sqrt((point[0]**2) + (point[1]**2))
+            return math.sqrt((point[0] ** 2) + (point[1] ** 2))
         
-        def helper(start, end):
+        def rand_quickselect(start, end):
+            # Base case
             if start >= end:
-                return 
+                return
             
             pivot_idx = random.randint(start, end)
             (points[start], points[pivot_idx]) = (points[pivot_idx], points[start])
-            pivot = get_distance(points[start])
-            # Lomuto's partitioning 
+            pivot_dist = get_distance(points[start])
+            
+            # Lomuto's partitioning
             left = start
             for right in range(start+1, end+1):
-                if get_distance(points[right]) <= pivot:
+                if get_distance(points[right]) <= pivot_dist:
                     left += 1
                     (points[left], points[right]) = (points[right], points[left])
             
+            # Place pivot in its right pos
             (points[start], points[left]) = (points[left], points[start])
             
             if left == k:
                 return
             elif left < k:
-                return helper(left + 1, end)
+                return rand_quickselect(left+1, end)
             else:
-                return helper(start, left - 1)
-            
-            
+                return rand_quickselect(start, left-1)
         
-        helper(0, len(points) - 1)
+        
+        N = len(points)
+        rand_quickselect(0, N-1)
         return points[:k]
