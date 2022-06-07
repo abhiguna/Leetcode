dba7b129767fd7e9df2a36983d6837b111a73b41
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 18:59:42 -0400

    Time: 58 ms (95.90%), Space: 18.1 MB (49.76%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,37 +1,30 @@
-from collections import *
-
-# Time = O(1) 
+# Time = O(1)
 # Space = O(1)
 class MinStack:
-
     def __init__(self):
-        self.my_stack = deque()
-        self.size = 0
-        self.min_stack = deque()
+        self.push_stack = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
-        # If the stack is empty, push the current value into the min_stack
-        if self.size == 0:
+        self.push_stack.append(val)
+        
+        if len(self.min_stack) == 0:
             self.min_stack.append(val)
         else:
-        # Otherwise, append the minimum of the current value and the minimum seen thus far
-            self.min_stack.append(min(val, self.min_stack[-1]))
-        self.my_stack.append(val)
-        self.size += 1
+            self.min_stack.append(min(self.min_stack[-1], val))
         return
-            
+        
+
     def pop(self) -> None:
         self.min_stack.pop()
-        self.my_stack.pop()
-        self.size -= 1
+        self.push_stack.pop()
         return
 
     def top(self) -> int:
-        return self.my_stack[-1]
-
+        return self.push_stack[-1]
+        
     def getMin(self) -> int:
         return self.min_stack[-1]
-        
 
 
 # Your MinStack object will be instantiated and called as such:
