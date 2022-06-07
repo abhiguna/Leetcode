a54bc7a826e49eda62950375d4e833c1e9851e00
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 15:34:28 -0400

    Time: 54 ms (19.28%), Space: 13.8 MB (56.24%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,17 +1,18 @@
 class Solution:
-    
     # Time = O(N)
     # Space = O(1)
     def climbStairs(self, n: int) -> int:
+        # Edge case
         if n == 1:
             return 1
         
+        # Define the base cases
         first = 1
         second = 2
         
-        for i in range(3, n + 1):
+        for i in range(3, n+1):
             third = first + second
             first = second
             second = third
-            
+        
         return second
