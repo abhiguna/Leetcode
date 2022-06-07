1a6aa00fb748cb8deb2973b62d6728f61dbe0a50
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 19:44:08 -0400

    Time: 548 ms (21.95%), Space: 16.3 MB (90.68%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,46 +4,42 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         M, N = len(grid), len(grid[0])
         
-        def get_neighbors(curr_row, curr_col):
+        def get_neighbors(row, col):
             neighbors = []
-            if curr_row + 1 < M:
-                neighbors.append((curr_row+1, curr_col))
-            if curr_row - 1 >= 0:
-                neighbors.append((curr_row-1, curr_col))
-            if curr_col - 1 >= 0:
-                neighbors.append((curr_row, curr_col-1))
-            if curr_col + 1 < N:
-                neighbors.append((curr_row, curr_col+1))
-            
+            if row + 1 < M:
+                neighbors.append((row+1, col))
+            if col + 1 < N:
+                neighbors.append((row, col+1))
+            if row - 1 >= 0:
+                neighbors.append((row-1, col))
+            if col - 1 >= 0:
+                neighbors.append((row, col-1))
             return neighbors
             
             
-        
-        def bfs(src_row, src_col):
+        def bfs(row, col):
             queue = deque()
-            queue.append((src_row, src_col))
-            grid[src_row][src_col] = "0"
+            queue.append((row, col))
+            grid[row][col] = "0"
             
             while queue:
                 (curr_row, curr_col) = queue.popleft()
                 
-                for (nei_row, nei_col) in get_neighbors(curr_row, curr_col):
-                    if grid[nei_row][nei_col] == "1":
-                        grid[nei_row][nei_col] = "0"
-                        queue.append((nei_row, nei_col))
-                
+                for (nrow, ncol) in get_neighbors(curr_row, curr_col):
+                    if grid[nrow][ncol] == "1":
+                        grid[nrow][ncol] = "0"
+                        queue.append((nrow, ncol))
+            
             return
-            
-            
         
-        # Find the number of connected components in the graph
-        components = 0
-        for row in range(M):
-            for col in range(N):
-                if grid[row][col] == "1":
-                    components += 1
-                    bfs(row, col)
         
-        return components
-            
-            
+        # Outer loop
+        islands = 0
+        for r in range(M):
+            for c in range(N):
+                # If not visited -> BFS
+                if grid[r][c] == "1":
+                    islands += 1
+                    bfs(r, c)
+        
+        return islands
