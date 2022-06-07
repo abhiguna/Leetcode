2cc6dbdf32b24211cc0d19d444f4822a6548bd6d
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-06 23:40:27 -0400

    Time: 76 ms (37.57%), Space: 14.3 MB (99.21%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,25 +1,22 @@
 class Solution:
-    # Approach: Two-pointer approach
-    
     # Time = O(N)
     # Space = O(1)
     def isPalindrome(self, s: str) -> bool:
         N = len(s)
-        i = 0
-        j = N - 1
-        # str of len 0 and 1 -> is a palindrome
-        while i < j:
-            # Skip all non-alphanumeric chars
-            if not s[i].isdigit() and not s[i].isalpha():
-                i += 1
-            elif not s[j].isdigit() and not s[j].isalpha():
-                j -= 1
+        left = 0
+        right = N - 1
+        
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
             else:
-                # We compare alphanumeric characters
-                if s[i].lower() != s[j].lower():
+                # Not palindromic case
+                if s[left].lower() != s[right].lower():
                     return False
-                else:
-                    i += 1
-                    j -= 1
+                left += 1
+                right -= 1
         
         return True
+        
