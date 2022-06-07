3b1a1e064c29f9e0b9733d06b9cb1db48cc48ae7
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 15:18:59 -0400

    Time: 1789 ms (61.59%), Space: 14.2 MB (47.71%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,23 +1,26 @@
-import math
-
 class Solution:
-    # Approach: DP
-    
-    # Time = O(A*C)
+    # Time = O(A*C), C: len(coins), A: amount
     # Space = O(A)
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # Edge case:
+        if amount == 0:
+            return 0
+        
         C = len(coins)
         A = amount
-        table = [math.inf for _ in range(A+1)]
-        # Base case
+        
+        table = [math.inf] * (A+1)
         table[0] = 0
         
         for a in range(1, A+1):
             for c in coins:
-                if (a - c) >= 0:
+                if c <= a:
                     table[a] = min(table[a], 1 + table[a-c])
         
         if table[A] == math.inf:
             return -1
         
         return table[A]
+        
+        
+        
