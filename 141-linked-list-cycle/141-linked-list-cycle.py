8b9c056317c0ce7d573277c9d714236f1af0b85a
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 10:28:25 -0400

    Time: 71 ms (58.45%), Space: 17.6 MB (65.44%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -8,17 +8,12 @@ class Solution:
     # Time = O(N)
     # Space = O(1)
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # Edge case: the list is empty
-        if not head: return False
-        
         hare, tortoise = head, head
-        # While the hare can move two steps forward
-        while hare.next and hare.next.next:
+        while hare and hare.next:
             hare = hare.next.next
             tortoise = tortoise.next 
-            # Check if they meet at some point
-            while hare == tortoise:
+            
+            if hare == tortoise:
                 return True
         
-        # Reached the end of the linked list
         return False
