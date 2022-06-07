55595e7b1dbf76bab489759602ec2fd4f967d1d6
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 19:32:29 -0400

    Time: 58 ms (57.62%), Space: 17 MB (25.16%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -4,29 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-import math
-
 class Solution:
+    # Time = O(N), N: # of nodes in the tree
+    # Space = O(H), H: height of the tree
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        is_bst = [True]
         
-        def dfs(node, lower_bound, upper_bound):
-            # Base case
-            if not node:
-                return True
+        def dfs(node, lower, upper):
+            # Preorder: N-L-R
+            condition = lower < node.val < upper
+            if not condition:
+                is_bst[0] = False
             
-            # Recursive case 
-            if node.val <= lower_bound or node.val >= upper_bound:
-                return False
-            
-            is_left_valid = True
-            is_right_valid = True
+            # Base case: leaf node
+            if not node.left and not node.right:
+                return
             
             if node.left:
-                is_left_valid = dfs(node.left, lower_bound, node.val)
-            
+                dfs(node.left, lower, node.val)
             if node.right:
-                is_right_valid = dfs(node.right, node.val, upper_bound)
-            
-            return is_left_valid and is_right_valid
+                dfs(node.right, node.val, upper)
+            return
         
-        return dfs(root, -math.inf, math.inf)
+        dfs(root, -math.inf, math.inf)
+        return is_bst[0]
