dc54cf1b7542e34dfe912d836df507c0a74067e8
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 13:23:09 -0400

    Time: 136 ms (15.00%), Space: 14.2 MB (99.80%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,31 +1,26 @@
-# Time = O(N)
-# Space = O(N)
 class Solution:
-    def evaluate(self, op1, op2, operator):
-        if operator == "+":
-            return op1 + op2
-        elif operator == "-":
-            return op1 - op2
-        elif operator == "*":
-            return op1 * op2
-        else:
-            # Integer division
-            return int(op1/op2)
-
-        
+    # Time = O(N)
+    # Space = O(N)
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = deque()
-        ops = set(["+", "-", "*", "/"])
+        N = len(tokens)
+        stack = []
         
-        for token in tokens:
-            if token not in ops:
-                # Operand
-                stack.append(int(token))
-            else:
+        for t in tokens:
+            # Operator
+            if t in ["+", "-", "*", "/"]:
                 op2 = stack.pop()
                 op1 = stack.pop()
-                operator = token
-                stack.append(self.evaluate(op1, op2, operator))
+                if t == '+':
+                    stack.append(op1 + op2)
+                elif t == '-':
+                    stack.append(op1 - op2)
+                elif t == '*':
+                    stack.append(op1 * op2)
+                else:
+                    stack.append(int(op1 / op2))
+            # Operand
+            else:
+                stack.append(int(t))
+                
+        return stack[-1]
         
-        # Result will be in the top of the stack
-        return stack.pop()
