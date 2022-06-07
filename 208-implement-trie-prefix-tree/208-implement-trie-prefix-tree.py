ce7607b8f411b10268d50d11a3c43a1773a3f87a
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 14:09:35 -0400

    Time: 306 ms (27.24%), Space: 31.7 MB (36.94%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,49 +1,41 @@
-class TrieNode:
+# For inserting, searching, startingWith a string of length N
+# Time = O(N)
+# Space = O(N)
+class Trie:
     def __init__(self):
         self.children = {}
         self.is_end_of_word = False
-    
-class Trie:
+        
 
-    def __init__(self):
-        self.root = TrieNode()
-    
-    # Time = O(N), N: len(word)
-    # Space = O(N)
     def insert(self, word: str) -> None:
-        curr = self.root
+        curr = self
+        for i in range(len(word)):
+            if word[i] not in curr.children:
+                curr.children[word[i]] = Trie()
+            curr = curr.children[word[i]]
         
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        
+        # Set the last node to end of the word
         curr.is_end_of_word = True
         return
-    
-    # Time = O(N)
-    # Space = O(1)
+                
     def search(self, word: str) -> bool:
-        curr = self.root
-        
-        for c in word:
-            if c not in curr.children:
+        curr = self
+        for i in range(len(word)):
+            if word[i] not in curr.children:
                 return False
-            curr = curr.children[c]
+            curr = curr.children[word[i]]
         
         return curr.is_end_of_word
+        
 
-    # Time = O(M), M: len(prefix)
-    # Space = O(1)
     def startsWith(self, prefix: str) -> bool:
-        curr = self.root 
-        
-        for c in prefix:
-            if c not in curr.children:
+        curr = self
+        for i in range(len(prefix)):
+            if prefix[i] not in curr.children:
                 return False
-            curr = curr.children[c]
-        
+            curr = curr.children[prefix[i]]
         return True
+        
 
 
 # Your Trie object will be instantiated and called as such:
