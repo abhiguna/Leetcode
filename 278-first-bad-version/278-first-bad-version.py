3e4fb9bc73f049a2f94a537a6076bd1e77d4beb9
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-07 10:44:12 -0400

    Time: 54 ms (18.45%), Space: 13.9 MB (12.14%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -2,19 +2,18 @@
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
-    # Time = O(logN)
+    # Time = O(logn)
     # Space = O(1)
     def firstBadVersion(self, n: int) -> int:
         start = 1
         end = n
-        while start <= end:
-            mid = start + (end - start) // 2
-            if isBadVersion(mid):
-                end = mid - 1
-            else:
-                start = mid + 1
         
-        # Let x be the boundary between the last good product and the first bad product
-        # Then start will be the first element > x
-        # And end will be the last element < x
+        while start <= end:
+            mid = start + (end-start) // 2
+            
+            if isBadVersion(mid) == False:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
         return start
