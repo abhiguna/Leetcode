bd28e2ec002e14c1adf2f8a50a6bffc6e15282a9
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 06:14:22 -0400

    Time: 173 ms (20.69%), Space: 14.2 MB (13.55%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,38 +1,37 @@
 class Solution:
-    # Time = O(N*2^N)
-    # Space = O(N)
+    # Time = O(T*2^N)
+    # Space = O(T)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         N = len(candidates)
+        T = target
         res = []
-        candidates.sort()
         
-        def helper(idx, target_left, slate):
-            # Backtracking case
-            if (target_left < 0) or (idx == N and target_left != 0):
-                return
-            
+        def helper(idx, combination, target_left):
             # Base case
             if target_left == 0:
-                res.append(slate[:])
+                res.append(combination[:])
                 return
             
-            # General case
-            # Exclude
-            helper(idx+1, target_left, slate)
+            if idx == N or target_left < 0:
+                return
             
-            # Include
+            # Recursive Case
             count = target_left // candidates[idx]
-            for i in range(1, count+1):
-                for j in range(i):
-                    slate.append(candidates[idx])
-                    target_left -= candidates[idx]
-                helper(idx+1, target_left, slate)
-                for j in range(i):
-                    slate.pop()
-                    target_left += candidates[idx]
-    
-            return
             
+            # Exclude case
+            helper(idx+1, combination, target_left)
+            
+            # Include case
+            for i in range(1, count+1):
+                for j in range(1, i+1):
+                    combination.append(candidates[idx])
+                    target_left -= candidates[idx]
+                helper(idx+1, combination, target_left)
+                for j in range(1, i+1):
+                    combination.pop()
+                    target_left += candidates[idx]
+                    
+            return
         
-        helper(0, target, [])
+        helper(0, [], target)
         return res
