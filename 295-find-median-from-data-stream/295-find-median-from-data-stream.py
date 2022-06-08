7264d522f4ddea5a9df66b433bae398f23db7334
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 11:23:29 -0400

    Time: 558 ms (89.67%), Space: 36.2 MB (35.44%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,41 +1,45 @@
+# Time = O(logN) for addNum and O(1) for findMedian
+# Space = O(1) for each element from the data stream
+
 class MedianFinder:
-    # Time = O(logN) for the Nth number
-    # Space = O(N)
     def __init__(self):
-        self.min_heap = []
         self.max_heap = []
-        self.size = 0
+        self.min_heap = []
 
     def rebalance_heaps(self):
-        if len(self.max_heap) > len(self.min_heap):
-            heappush(self.min_heap, -heappop(self.max_heap))
-        else:
+        if len(self.min_heap) > len(self.max_heap):
             heappush(self.max_heap, -heappop(self.min_heap))
-        
+        else:
+            heappush(self.min_heap, -heappop(self.max_heap))
+
+    
     def addNum(self, num: int) -> None:
-        if self.size == 0 or num <= -self.max_heap[0]:
+        # Edge case: max heap is empty
+        if len(self.max_heap) == 0:
+            heappush(self.max_heap, -num)
+            return
+        
+        if num <= abs(self.max_heap[0]):
             heappush(self.max_heap, -num)
         else:
             heappush(self.min_heap, num)
         
-        self.size += 1
-        
         if abs(len(self.max_heap) - len(self.min_heap)) > 1:
             self.rebalance_heaps()
+        return
         
 
     def findMedian(self) -> float:
-        # The number of elements is odd
-        if self.size % 2 == 1:
-            if len(self.min_heap) > len(self.max_heap):
-                return self.min_heap[0]
-            else:
-                return -self.max_heap[0]
-                
-        # The number of elements is even
-        else:
+        # Even number of elements inserted
+        if len(self.min_heap) == len(self.max_heap):
             return (self.min_heap[0] + -self.max_heap[0]) / 2.0
         
+        # Odd number of elements inserted
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        
+        if len(self.min_heap) > len(self.max_heap):
+            return self.min_heap[0]
 
 
 # Your MedianFinder object will be instantiated and called as such:
