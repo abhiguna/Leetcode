577d7725639e66a79fffbdf22be0b0f6b74a57e8
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 12:52:27 -0400

    Time: 38 ms (65.20%), Space: 13.9 MB (54.72%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -4,19 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # Approach: Hare and tortoise pattern
-    
     # Time = O(N)
     # Space = O(1)
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         hare, tortoise = head, head
+        
         while hare.next and hare.next.next:
             hare = hare.next.next
             tortoise = tortoise.next 
         
-        # Edge case: list is even len
+        # Edge case: even number of nodes 
         if hare.next:
-            tortoise = tortoise.next 
-        return tortoise
-    
+            return tortoise.next 
         
+        return tortoise
+            
