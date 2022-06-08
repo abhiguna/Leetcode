7498e655899bc3051d2ac15c9569891bec027559
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 18:04:41 -0400

    Time: 48 ms (47.31%), Space: 13.9 MB (63.07%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -6,23 +6,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         N = len(nums)
-        red, white, blue = 0, 0, N-1
+        red = 0
+        white = 0
+        blue = N - 1
         
         while white <= blue:
-            if nums[white] == 0:
+            if nums[white] == 1:
+                white += 1
+            elif nums[white] == 0:
                 # Swap with red
-                (nums[red], nums[white]) = (nums[white], nums[red])
+                nums[red], nums[white] = nums[white], nums[red]
                 red += 1
                 white += 1
-            elif nums[white] == 2:
-                # Swap with blue
-                (nums[blue], nums[white]) = (nums[white], nums[blue])
-                blue -= 1
-                # DON'T increment the white ptr
             else:
-                white += 1
+                # Swap with blue
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
         
-        return nums
-        
-        
-        
+        return
