857e992bdab789a076149b0447a73b46fa4bb530
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 10:42:15 -0400

    Time: 245 ms (11.30%), Space: 16 MB (39.75%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -3,26 +3,32 @@ class Solution:
     # Space = O(1)
     def trap(self, height: List[int]) -> int:
         N = len(height)
+        
+        # Edge case: N <= 2
+        if N <= 2:
+            return 0
+        
         trapped_vol = 0
-        curr_level = 0
+        prev_level = 0
         i = 0
         j = N - 1
         
         while i < j - 1:
-            my_level = min(height[i], height[j])
+            curr_level = min(height[i], height[j])
             
-            # Increase trapped volume only when there is an increase in height
-            if my_level > curr_level:
-                trapped_vol += (my_level-curr_level) * (j-i-1)
-                curr_level = my_level
+            # Add volume if possible
+            if curr_level > prev_level:
+                height_diff = curr_level - prev_level
+                trapped_vol += (height_diff) * (j-i-1)
+                prev_level = curr_level
             
+            # Subtract volume by the height of the wall
             if height[i] <= height[j]:
                 i += 1
-                # Decrement trapped volume
-                trapped_vol -= min(curr_level, height[i])
+                trapped_vol -= min(prev_level, height[i])
             else:
                 j -= 1
-                # Decrement trapped volume
-                trapped_vol -= min(curr_level, height[j])
+                trapped_vol -= min(prev_level, height[j])
+                
         
         return trapped_vol
