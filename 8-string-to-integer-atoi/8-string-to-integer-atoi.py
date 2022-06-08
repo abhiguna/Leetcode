62ada3e1cb8a1a6cd2d7c0cca29f4cdf2ee92c50
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 19:43:17 -0400

    Time: 61 ms (25.36%), Space: 13.8 MB (78.55%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,48 +1,58 @@
-# Optimal
-
-# Date: 2/6/22
-# 30m 3
 class Solution:
-    # Pattern ad-hoc
-    # Time = O(n)
+    # Time = O(1)
     # Space = O(1)
     def myAtoi(self, s: str) -> int:
-        sign = 1
-        result = 0
-        idx = 0
         N = len(s)
+        # Edge case
+        if N == 0:
+            return 0
         
-        INT_MAX = pow(2, 31) - 1
-        INT_MIN = -pow(2, 31)
+        def check_overflow(ans, d, limit):
+            limit = (limit - d) / 10
+            if ans >= limit:
+                return True
+            return False
+            
         
-        # Discard all spaces from beginning of the string
-        while idx < N and s[idx] == ' ':
+        is_positive = True
+        
+        # Process until digit arrives
+        idx = 0
+        
+        # Ignore leading whitespace
+        while idx < N and s[idx] == " ":
             idx += 1
         
-        # Update the sign of the number. 
-        if idx < N and s[idx] == '-':
-            sign = -1
-            idx += 1
-        elif idx < N and s[idx] == '+':
-            sign = 1
+        # Process sign char if exists
+        if idx < N and (s[idx] == "+" or s[idx] == "-"):
+            if s[idx] == "-":
+                is_positive = False
             idx += 1
         
-        # Traverse the string until a non-digit character is encountered
+        # Edge case: check if non-digit chars present
+        while idx < N and not s[idx].isdigit():
+            return 0
+        
+        # Process all the digits
+        res = 0
         while idx < N and s[idx].isdigit():
-            digit = int(s[idx])
+            digit = ord(s[idx]) - ord("0")
             
-            # Check overflow and underflow conditions
-            if ((result > INT_MAX // 10) or (result == INT_MAX // 10 and digit > INT_MAX % 10)):
-                # Return number based on overflow or underflow
-                return INT_MAX if sign == 1 else INT_MIN
+            # Check overflow
+            if is_positive:
+                if check_overflow(res, digit, (2**31)-1):
+                    return (2**31) - 1
+            else:
+                if check_overflow(res, digit, (2**31)):
+                    return -1*(2**31)
             
-            # Append current digit
-            result = result * 10 + digit
+            # No overflow possible
+            res = (res * 10) + digit
             idx += 1
         
-        # Return final result number after updating its sign
-        return sign * result
-                
+        # Process sign and return
+        return res if is_positive else -1*res
         
         
-            
+        
+        
