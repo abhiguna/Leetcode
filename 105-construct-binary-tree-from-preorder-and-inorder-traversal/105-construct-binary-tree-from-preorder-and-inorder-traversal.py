de326bfa1b7cd71e10fdfddfe5232266438f71b1
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-09 15:44:53 -0400

    Time: 91 ms (78.93%), Space: 18.8 MB (89.53%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,25 +8,28 @@ class Solution:
     # Time = O(N)
     # Space = O(N)
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        idx_map = defaultdict(int)
+        # Provides constant lookup
+        inorder_map = {}
+        for (idx, val) in enumerate(inorder):
+            inorder_map[val] = idx
         
-        for idx, val in enumerate(inorder):
-            idx_map[val] = idx
-        
-        preorder_idx = 0
+        preorder_idx = [0]
         
         def dfs(start, end):
-            nonlocal preorder_idx 
-            
-            # Base case
+            # Base case: empty or singleton array
             if start > end:
                 return None
+
+            # Recursive case: internal node
+            rval = preorder[preorder_idx[0]]
+            preorder_idx[0] += 1
+            node = TreeNode(rval)
             
-            root = TreeNode(preorder[preorder_idx])
-            preorder_idx += 1
+            root_idx = inorder_map[rval]
             
-            root.left = dfs(start, idx_map[root.val] - 1)
-            root.right = dfs(idx_map[root.val] + 1, end)
-            return root
-        
-        return dfs(0, len(preorder) - 1)
+            node.left = dfs(start, root_idx-1)
+            node.right = dfs(root_idx+1, end)
+            return node
+            
+        root = dfs(0, len(preorder) - 1)
+        return root
