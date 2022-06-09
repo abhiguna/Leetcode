29f57da145ece92822dd0f19bc1f83781395683d
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-09 06:48:39 -0400

    Time: 200 ms (66.87%), Space: 17.5 MB (46.69%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,53 +1,45 @@
 class Solution:
-    # Time = O(N*M^2) N: len(wordList), M: len(wordList[0])
-    # Space = O(N^2*M)
-    
+    # Time = O(N*M^2)
+    # Space = O(N*M^2)
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # Add beginWord to list
+        # Edge case
+        if endWord not in wordList:
+            return 0
+        
+        adj_list = defaultdict(list)
         wordList.append(beginWord)
-        word_set = set(wordList)
-        adj_list = defaultdict(set)
-        visited = set()
         
         def build_graph():
-            for i in range(len(wordList)):
-                for j in range(0, len(wordList[i])):
-                    for c in "abcdefghijklmnopqrstuvwxyz":
-                        word = wordList[i]
-                        t_word = word[:j] + c + word[j+1:]
-                        if t_word in word_set:
-                            adj_list[word].add(t_word)
-            return
-        
-        # Finds the shortest path len
-        path_len = [1]
+            for word in wordList:
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    adj_list[pattern].append(word)
+
+        visited = set()
         
         def bfs():
             queue = deque()
             queue.append(beginWord)
             visited.add(beginWord)
+            words_in_path = 1
             
             while queue:
                 num_nodes = len(queue)
-                
-                # Process nodes level-by-lvel
+                # Traverse layer by layer
                 for i in range(num_nodes):
                     node = queue.popleft()
-                
                     if node == endWord:
-                        return path_len[0]
-
-                    for nei in adj_list[node]:
-                        if nei not in visited:
-                            visited.add(nei)
-                            queue.append(nei)
-
-                path_len[0] += 1
-    
+                        return words_in_path
+                    for j in range(len(node)):
+                        pattern = node[:j] + "*" + node[j+1:]
+                        for nei in adj_list[pattern]:
+                            if nei not in visited:
+                                visited.add(nei)
+                                queue.append(nei)
+                
+                words_in_path += 1
             return 0
         
         build_graph()
         return bfs()
-        
-        
         
