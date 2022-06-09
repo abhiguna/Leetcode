a090ede3acc4328a48890aeb88b08ee0fc9e353a
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-09 18:47:16 -0400

    Time: 9010 ms (13.97%), Space: 13.9 MB (92.58%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,34 +1,28 @@
 class Solution:
-    # Time = O(N*M*4^L), L: len(word)
-    # Space = O(L)
+    # Time = O(4^(M+N))
+    # Space = O(M+N)
     def exist(self, board: List[List[str]], word: str) -> bool:
-        M = len(board)
-        N = len(board[0])
+        M, N = len(board), len(board[0])
         visited = set()
         
-        def dfs(row, col, idx):
+        def dfs(idx, row, col):
             # Base case
             if idx == len(word):
                 return True
-            if row < 0 or col < 0 or row >= M or col >= N or board[row][col] != word[idx] or \
-                (row, col) in visited:
+            # Backtracking case
+            if row < 0 or row >= M or col < 0 or col >= N or word[idx] != board[row][col] or (row, col) in visited:
                 return False
             
+            # General case
             visited.add((row, col))
-            res = dfs(row+1, col, idx+1) or \
-                    dfs(row-1, col, idx+1) or \
-                    dfs(row, col+1, idx+1) or \
-                    dfs(row, col-1, idx+1)
+            res = dfs(idx+1, row+1, col) or dfs(idx+1, row-1, col) or dfs(idx+1, row, col-1) or dfs(idx+1, row, col+1)
             visited.remove((row, col))
             return res
-                
-        for r in range(M):
-            for c in range(N):
-                word_found = dfs(r, c, 0)
-                if word_found: return True
         
+        # Outer loop
+        for i in range(M):
+            for j in range(N):
+                found = dfs(0, i, j)
+                if found: return True
         return False
-        
-        
-        
-            
+    
