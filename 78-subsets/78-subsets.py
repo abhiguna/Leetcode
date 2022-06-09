95c981039a5053b59cbbc9f7728f75462800709f
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-08 20:48:43 -0400

    Time: 30 ms (97.10%), Space: 14.2 MB (32.74%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -2,25 +2,23 @@ class Solution:
     # Time = O(N*2^N)
     # Space = O(N)
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # Subsets can be reduced to the combinations problem
-        res = []
         N = len(nums)
+        res = []
         
         def helper(idx, slate):
             # Base case
             if idx == N:
                 res.append(slate[:])
-            else:
-                # Exclude the current element
-                helper(idx+1, slate)
-                
-                # Include the current element
-                slate.append(nums[idx])
-                helper(idx+1, slate)
-                slate.pop()
+                return
+
+            # Exclude case
+            helper(idx+1, slate)
             
+            # Include case
+            slate.append(nums[idx])
+            helper(idx+1, slate)
+            slate.pop()
             return
-        
-        # Root manager
+            
         helper(0, [])
         return res
