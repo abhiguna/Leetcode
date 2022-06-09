58695378ef01fc9dbe763c41264eebe2b852bb6b
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-09 15:19:48 -0400

    Time: 42 ms (57.27%), Space: 13.8 MB (73.30%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,11 +1,11 @@
 class Solution:
     # Time = O(m*n)
-    # Space = O(m*n)
+    # Space = O(n)
     def uniquePaths(self, m: int, n: int) -> int:
-        table = [[1 for j in range(n)] for i in range(m)]
+        table = [[1 for j in range(n)] for i in range(2)]
         
         for i in range(1, m):
             for j in range(1, n):
-                table[i][j] = table[i-1][j] + table[i][j-1]
+                table[i%2][j] = table[(i-1)%2][j] + table[(i%2)][j-1]
         
-        return table[m-1][n-1]
+        return table[(m-1)%2][n-1]
