3a4d774ee5df67b709e8d3b18051800c9207ef8e
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-10 07:41:36 -0400

    Time: 63 ms (48.27%), Space: 18 MB (41.83%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -5,25 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # Time = O(N)
-    # Space = O(N)
+    # Time = O(N), N: # of nodes in the tree
+    # Space = O(h), h: height of the tree
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        count = 0
-        stack = []
-        curr = root
+        kth_smallest = [-1]
+        num_nodes = [0]
         
-        while stack or curr:
-            # Left subtree
-            while curr:
-                stack.append(curr)
-                curr = curr.left 
+        def dfs(node):
+            # Base case:
+            if not node.left and not node.right:
+                num_nodes[0] += 1
+                if num_nodes[0] == k:
+                    kth_smallest[0] = node.val
+                return
             
-            # Node
-            curr = stack.pop()
-            count += 1
-            if count == k:
-                return curr.val 
+            # General case: inorder traversal L-N-R
+            if node.left:
+                dfs(node.left)
             
-            curr = curr.right 
-        
-        return -1
+            num_nodes[0] += 1
+            if num_nodes[0] == k:
+                kth_smallest[0] = node.val 
+            
+            if node.right:
+                dfs(node.right)
+            
+            
+        dfs(root)
+        return kth_smallest[0]
