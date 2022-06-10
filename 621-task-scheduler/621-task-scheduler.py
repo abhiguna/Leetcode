097ecf76081c26273d8898eb2e1aef0430ab161f
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-09 22:58:04 -0400

    Time: 932 ms (22.11%), Space: 14.5 MB (36.22%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,27 +1,35 @@
+from heapq import *
+from collections import *
+
 class Solution:
-    # Time = O(m*n), m = len(tasks) and n = idle time
-    # Space = O(1) since fixed set of characters
+    # Time = O(N)
+    # Space = O(1)
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # each task 1 unit time
-        # minimize idle time
+        task_count = Counter(tasks)
+        # Insert values of each char into the max_heap
+        max_heap = [ -cnt for cnt in task_count.values()]
+        heapify(max_heap)
         
-        count = Counter(tasks)
-        max_heap = [-cnt for cnt in count.values()]
-        heapq.heapify(max_heap)
+        # Task queue keeps track of tasks to be processed (-cnt, ready_time)
+        task_queue = deque()
+        # Holds the current time
+        timestamp = 0
         
-        curr_time = 0
-        queue = deque() # contains pair of values [-cnt, idleTime]
-        while max_heap or queue:
-            curr_time += 1
+        while max_heap or task_queue:
+            timestamp += 1
             
             if max_heap:
-                cnt = 1 + heapq.heappop(max_heap)
-                if cnt:
-                    queue.append([cnt, curr_time + n])
-            
-            if queue and queue[0][1] == curr_time:
-                heapq.heappush(max_heap, queue.popleft()[0])
-        
-        return curr_time
+                cnt = heappop(max_heap)
+                # Update cnt -> one less instance available for future use
+                cnt = -1*(abs(cnt) - 1)
+                # Add to the queue if at least one instance is available
+                if cnt != 0:
+                    task_queue.append((cnt, timestamp + n))
                 
+            # Process if a task is ready
+            if task_queue and task_queue[0][1] == timestamp:
+                (cnt, ready_time) = task_queue.popleft()
+                heappush(max_heap, cnt)
         
+        return timestamp
+                
