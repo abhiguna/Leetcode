991facdbeaa4b61ac2df8f8a0f4119bb83bacb78
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-11 15:35:00 -0400

    Time: 35 ms (75.79%), Space: 14 MB (31.00%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -3,47 +3,48 @@ class Solution:
     # Space = O(1)
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         M, N = len(matrix), len(matrix[0])
-        r, c = 0, 0
-        prev_r, prev_c = 0, -1
-        num_elements = M*N
+        rem_elements = M*N
         res = []
         
-        while num_elements > 0:
+        # (0, 0) => (prev_r, prev_c+1) => (0, -1)
+        prev_r, prev_c = 0, -1
+        while rem_elements > 0:
             # Move right
-            r, c = prev_r, prev_c+1
-            while c < N and matrix[r][c] != -math.inf:
+            (r, c) = (prev_r, prev_c+1)
+            while (0 <= r < M) and (0 <= c < N) and matrix[r][c] != math.inf:
                 res.append(matrix[r][c])
-                matrix[r][c] = -math.inf 
-                num_elements -= 1
-                prev_r, prev_c = r, c
-                r, c = r, c+1
+                matrix[r][c] = math.inf 
+                rem_elements -= 1
+                (prev_r, prev_c) = (r, c)
+                (r, c) = (r, c+1)
             
             # Move down
-            r, c = prev_r+1, prev_c
-            while r < M and matrix[r][c] != -math.inf:
+            (r, c) = (prev_r+1, prev_c)
+            while (0 <= r < M) and (0 <= c < N) and matrix[r][c] != math.inf:
                 res.append(matrix[r][c])
-                matrix[r][c] = -math.inf
-                num_elements -= 1
-                prev_r, prev_c = r, c
-                r, c = r+1, c
+                matrix[r][c] = math.inf 
+                rem_elements -= 1
+                (prev_r, prev_c) = (r, c)
+                (r, c) = (r+1, c)
             
             # Move left
-            r, c = prev_r, prev_c - 1
-            while c >= 0 and matrix[r][c] != -math.inf:
+            (r, c) = (prev_r, prev_c-1)
+            while (0 <= r < M) and (0 <= c < N) and matrix[r][c] != math.inf:
                 res.append(matrix[r][c])
-                matrix[r][c] = -math.inf
-                num_elements -= 1
-                prev_r, prev_c = r, c
-                r, c = r, c-1
+                matrix[r][c] = math.inf 
+                rem_elements -= 1
+                (prev_r, prev_c) = (r, c)
+                (r, c) = (r, c-1)
             
             # Move up
-            r, c = prev_r-1, prev_c
-            while r >= 0 and matrix[r][c] != -math.inf:
+            (r, c) = (prev_r-1, prev_c)
+            while (0 <= r < M) and (0 <= c < N) and matrix[r][c] != math.inf:
                 res.append(matrix[r][c])
-                matrix[r][c] = -math.inf 
-                num_elements -= 1
-                prev_r, prev_c = r, c
-                r, c = r-1, c
+                matrix[r][c] = math.inf 
+                rem_elements -= 1
+                (prev_r, prev_c) = (r, c)
+                (r, c) = (r-1, c)
         
         return res
+        
         
