7ebc578836601707355d064d5a574879292688c2
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-11 15:11:27 -0400

    Time: 1136 ms (25.02%), Space: 17 MB (77.99%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,30 +1,30 @@
 class Solution:
-    # Time = O(M*N)
-    # Space = O(1)
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         M, N = len(mat), len(mat[0])
-        dist = [[math.inf for j in range(N)] for i in range(M)]
         
-        # First pass to update dist based on the top and left values
+        # table[r][c] stores nearest dist to a 0
+        table = [[math.inf for j in range(N)] for i in range(M)]
+        
+        # First pass
         for r in range(M):
             for c in range(N):
                 if mat[r][c] == 0:
-                    dist[r][c] = 0
+                    table[r][c] = 0
                 else:
                     if r > 0:
-                        dist[r][c] = min(dist[r][c], 1 + dist[r-1][c])
+                        table[r][c] = min(table[r][c], 1 + table[r-1][c])
                     if c > 0:
-                        dist[r][c] = min(dist[r][c], 1 + dist[r][c-1])
+                        table[r][c] = min(table[r][c], 1 + table[r][c-1])
         
-        # Second reverse pass to update dist based on the bottom and right values
+        # Second pass
         for r in range(M-1, -1, -1):
             for c in range(N-1, -1, -1):
                 if mat[r][c] == 0:
-                    dist[r][c] == 0
+                    table[r][c] = 0
                 else:
                     if r < M-1:
-                        dist[r][c] = min(dist[r][c], 1 + dist[r+1][c])
+                        table[r][c] = min(table[r][c], 1 + table[r+1][c])
                     if c < N-1:
-                        dist[r][c] = min(dist[r][c], 1 + dist[r][c+1])
+                        table[r][c] = min(table[r][c], 1 + table[r][c+1])
         
-        return dist
+        return table
