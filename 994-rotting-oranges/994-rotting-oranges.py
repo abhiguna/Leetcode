832f5600161f471761e747b83a21ab48989ef58c
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-11 16:00:41 -0400

    Time: 61 ms (72.02%), Space: 14 MB (10.37%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,49 +1,46 @@
 class Solution:
     # Time = O(M*N)
-    # Space = O(max(M, N))
+    # Space = O(M+N)
     def orangesRotting(self, grid: List[List[int]]) -> int:
         M, N = len(grid), len(grid[0])
-        min_time = 0
-        fresh_oranges = 0
         
-        def get_neighbors(row, col):
-            neighbors = []
-            if row + 1 < M:
-                neighbors.append((row+1, col))
-            if col + 1 < N:
-                neighbors.append((row, col+1))
-            if row - 1 >= 0:
-                neighbors.append((row-1, col))
-            if col - 1 >= 0:
-                neighbors.append((row, col-1))
-            return neighbors
-            
+        total_minutes = 0
         queue = deque()
         
         for r in range(M):
             for c in range(N):
-                # Rotten orange
-                if grid[r][c] == 2: 
-                    queue.append((r, c))
-                # Fresh orange
-                elif grid[r][c] == 1:
-                    fresh_oranges += 1
+                if grid[r][c] == 2:
+                    queue.append((r, c, 0))
         
-        while queue and fresh_oranges > 0:
-            num_rotten = len(queue)
+        def get_neighbors(i, j):
+            neighbors = []
+            if i + 1 < M:
+                neighbors.append((i+1, j))
+            if j + 1 < N:
+                neighbors.append((i, j+1))
+            if i - 1 >= 0:
+                neighbors.append((i-1, j))
+            if j - 1 >= 0:
+                neighbors.append((i, j-1))
+            return neighbors
             
-            for i in range(num_rotten):
-                (row, col) = queue.popleft()
-                
-                for (nrow, ncol) in get_neighbors(row, col):
-                    if grid[nrow][ncol] == 1:
-                        grid[nrow][ncol] = 2
-                        fresh_oranges -= 1
-                        queue.append((nrow, ncol))
+
+        # Start a BFS
+        while queue:
+            (row, col, mins) = queue.popleft()
+            total_minutes = max(total_minutes, mins)
             
-            min_time += 1
+            for (nrow, ncol) in get_neighbors(row, col):
+                if grid[nrow][ncol] == 1:
+                    grid[nrow][ncol] = 2
+                    queue.append((nrow, ncol, mins+1))
         
-        if fresh_oranges > 0:
-            return -1
+        # Edge case: if impossible to make everything rotten
+        for r in range(M):
+            for c in range(N):
+                if grid[r][c] == 1:
+                    return -1
         
-        return min_time
+        return total_minutes
+                    
+        
