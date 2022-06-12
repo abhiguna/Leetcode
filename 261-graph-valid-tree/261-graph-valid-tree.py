5784d0968aff47fd5fa58a211deb865cd259e086
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-12 11:47:27 -0400

    Time: 103 ms (78.74%), Space: 17.3 MB (20.31%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -1,49 +1,44 @@
 class Solution:
-    # Time = O(M + N), M: # of edges in the graph, N: # of vertices in the graph
-    # Space = O(M + N)
+    # Time = O(m+n), m: len(edges), n: # of nodes
+    # Space = O(m+n)
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         adj_list = [[] for i in range(n)]
-        visited = [-1] * n
         parent = [-1] * n
+        visited = set()
         
-        # Build graph
         def build_graph():
             for (src, dest) in edges:
                 adj_list[src].append(dest)
                 adj_list[dest].append(src)
-            return
         
         build_graph()
         
-        # BFS for all unvisited nodes
-        def bfs(src):
-            queue = deque()
-            queue.append(src)
-            visited[src] = 1
+        def dfs(src):
+            visited.add(src)
             
-            while queue:
-                node = queue.popleft()
-                
-                for nei in adj_list[node]:
-                    if visited[nei] == -1:
-                        visited[nei] = 1
-                        parent[nei] = node
-                        queue.append(nei)
-                    elif parent[node] != nei:
+            for nei in adj_list[src]:
+                # Tree edge
+                if nei not in visited:
+                    parent[nei] = src
+                    has_cycle = dfs(nei)
+                    if has_cycle:
                         return True
-            
+                # Back edge
+                else:
+                    if parent[src] != nei:
+                        return True
             return False
         
-        components = 0
-        for i in range(n):
-            if visited[i] == -1:
-                components += 1
-                if components > 1:
+        num_components = 0
+        for v in range(n):
+            if v not in visited:
+                num_components += 1
+                # Graph not connected
+                if num_components > 1:
                     return False
-                
-                has_cycle = bfs(i)
+                has_cycle = dfs(v)
                 if has_cycle:
                     return False
         
-        # Check validity
         return True
+        
