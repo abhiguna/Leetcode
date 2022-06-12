aa8c64bc5ef21e26dd6b6dca78b729c30ab5f3bd
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-12 11:12:36 -0400

    Time: 127 ms (77.11%), Space: 14.2 MB (46.14%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,14 +1,32 @@
+# Note: This method does not generate a valid subsequence because it violates the order of the elements
+#.  But, it will correctly generate the length of the longest subsequence as the len only increases when n > sub[-1]
 class Solution:
-    # Time = O(n^2)
-    # Space = O(n)
+    # Time = O(nlogn)
+    # Space = O(1)
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        table = [1] * n
+        # Initialize a subsequence array
+        # We will also keep track of the largest len seen so far
+        sub = []
+        max_len = 0
         
-        for i in range(n-1, -1, -1):
-            for j in range(i+1, n):
-                # Check if increasing order
-                if nums[j] > nums[i]:
-                    table[i] = max(table[i], 1 + table[j])
+        # Iterate through every element n in the array
+        for n in nums:
+            # If sub empty or n > sub[-1] -> add it to the end of sub array
+            if len(sub) == 0 or n > sub[-1]:
+                sub.append(n)
+                max_len = max(max_len, len(sub))
+            # Otherwise, replace n with the first element greater than n in the sub array
+            else:
+                start = 0
+                end = len(sub) - 1
+                while start <= end:
+                    mid = start + (end-start) // 2
+                    if sub[mid] < n:
+                        start = mid + 1
+                    else:
+                        end = mid - 1
+                sub[start] = n  
+        # Return max_len
+        return max_len
         
-        return max(table)
+
