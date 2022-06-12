2fb1f400d9db5db41d64b434e597376776724bbb
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-12 09:26:27 -0400

    Time: 303 ms (9.34%), Space: 20.7 MB (77.68%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -2,8 +2,15 @@ class Solution:
     # Time = O(N)
     # Space = O(1)
     def countBits(self, n: int) -> List[int]:
-        count = []
-        for i in range(n + 1):
-            count.append(bin(i).count("1"))
+        res = []
+        for num in range(0, n+1):
+            one_bits = 0
+            
+            while num > 0:
+                # chew up at the lsb
+                one_bits += (num & 1)
+                num = num >> 1
+            
+            res.append(one_bits)
         
-        return count
+        return res
