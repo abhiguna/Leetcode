c525f0d744fadb1df5d3f5c9c3266d44b0508cf8
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-13 10:57:58 -0400

    Time: 47 ms (35.26%), Space: 14 MB (19.55%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    # Time = O(n), n: len(s)
+    # Space = O(n)
+    def decodeString(self, s: str) -> str:
+        stack = []
+        n = len(s)
+        
+        for i in range(n):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                # Keep popping all the letters
+                sub = deque()
+                while stack[-1] != "[":
+                    sub.appendleft(stack.pop())
+                
+                # Pop the open paran
+                stack.pop()
+                
+                # Get the digits
+                digit = deque()
+                while stack and stack[-1].isdigit():
+                    digit.appendleft(stack.pop())
+                
+                count = int("".join(digit))
+                sub = sub * count
+                stack.append("".join(sub))
+        
+        while len(stack) > 1:
+            sub = deque()
+            sub.appendleft(stack.pop())
+            sub.appendleft(stack.pop())
+            stack.append("".join(sub))
+        
+        return stack[-1]
+                
+        
