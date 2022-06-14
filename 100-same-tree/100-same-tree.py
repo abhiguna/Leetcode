41f788c08b4b312a8061742ccd2d5b7598c7de80
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-14 14:53:03 -0400

    Time: 48 ms (37.10%), Space: 14 MB (28.24%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,44 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    """
+    Time = O(n+m)
+        n: # of nodes in the tree rooted at p
+        m: # of nodes in the tree rooted at q
+    
+    Space = O(max{h(p), h(q)})
+        h(p): height of the tree rooted at p
+        h(q): heights of the tree rooted at q
+    """
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # Edge case: empty tree
+        # Base cases:
         if not p and not q:
             return True
         elif (p and not q) or (not p and q):
             return False
+        elif p.val != q.val:
+            return False
         
-        queue_p = deque([p])
-        queue_q = deque([q])
-        
-        while queue_p and queue_q:
-            num_nodes_p, num_nodes_q = len(queue_p), len(queue_q)
-            
-            # Structural check
-            if num_nodes_p != num_nodes_q:
-                return False
-            
-            for i in range(min(num_nodes_p, num_nodes_q)):
-                node_p = queue_p.popleft()
-                node_q = queue_q.popleft()
-                
-                # Check if values don't match
-                if node_p.val != node_q.val:
-                    return False
-                
-                # Check if structurally they don't match
-                if (node_p.left and not node_q.left) or (not node_p.left and node_q.left):
-                    return False
-                if (node_p.right and not node_q.right) or (not node_p.right and node_q.right):
-                    return False
-                
-                if node_p.left:
-                    queue_p.append(node_p.left)
-                    queue_q.append(node_q.left)
-                
-                if node_p.right:
-                    queue_p.append(node_p.right)
-                    queue_q.append(node_q.right)
-        
-        return True
-                    
+        # General case
+        left_same = self.isSameTree(p.left, q.left)
+        right_same = self.isSameTree(p.right, q.right)
+        return left_same and right_same
