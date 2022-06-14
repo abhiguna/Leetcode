74076736cc360f9ec33bf1113520a52dcf2e4f01
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-14 19:30:22 -0400

    Time: 4846 ms (45.15%), Space: 39.3 MB (44.13%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -23,27 +23,28 @@ class Solution:
         b_queue.append((x, y))
         
         while f_queue and b_queue:
-            # Process forward queue
-            (fi, fj) = f_queue.popleft()
-            
-            for (nfi, nfj) in get_neighbors(fi, fj):
-                if (nfi, nfj) not in f_visited:
-                    f_visited[(nfi, nfj)] = 1 + f_visited[(fi, fj)]
-                    f_queue.append((nfi, nfj))
-                    # Found answer
-                    if (nfi, nfj) in b_visited:
-                        return f_visited[(nfi, nfj)] + b_visited[(nfi, nfj)]
-            
-            # Process backward queue
-            (bi, bj) = b_queue.popleft()
-            
-            for (nbi, nbj) in get_neighbors(bi, bj):
-                if (nbi, nbj) not in b_visited:
-                    b_visited[(nbi, nbj)] = 1 + b_visited[(bi, bj)]
-                    b_queue.append((nbi, nbj))
-                    # Found answer
-                    if (nbi, nbj) in f_visited:
-                        return f_visited[(nbi, nbj)] + b_visited[(nbi, nbj)]
-            
+            if f_visited[f_queue[0]] < b_visited[b_queue[0]]:
+                # Process forward queue
+                (fi, fj) = f_queue.popleft()
+
+                for (nfi, nfj) in get_neighbors(fi, fj):
+                    if (nfi, nfj) not in f_visited:
+                        f_visited[(nfi, nfj)] = 1 + f_visited[(fi, fj)]
+                        f_queue.append((nfi, nfj))
+                        # Found answer
+                        if (nfi, nfj) in b_visited:
+                            return f_visited[(nfi, nfj)] + b_visited[(nfi, nfj)]
+            else:
+                # Process backward queue
+                (bi, bj) = b_queue.popleft()
+
+                for (nbi, nbj) in get_neighbors(bi, bj):
+                    if (nbi, nbj) not in b_visited:
+                        b_visited[(nbi, nbj)] = 1 + b_visited[(bi, bj)]
+                        b_queue.append((nbi, nbj))
+                        # Found answer
+                        if (nbi, nbj) in f_visited:
+                            return f_visited[(nbi, nbj)] + b_visited[(nbi, nbj)]
+
         # Will never reach here -> return placeholder
         return math.inf
