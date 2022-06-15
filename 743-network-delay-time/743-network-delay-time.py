df3d9b840ba7166c63e2e7b54a7661b21b95f5be
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-15 07:28:54 -0400

    Time: 486 ms (88.45%), Space: 16 MB (90.86%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,46 +1,46 @@
 class Solution:
-    # Time = O(MlogN)
-    # Space = O(M + N)
+    # Time = O(mlog(m)) -> O(mlog(n)), m: len(times), n: # of nodes
+    # Space = O(m)
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        dist = [math.inf for _ in range(n+1)]
-        dist[k] = 0
-        dist[0] = 0
-        
-        adj_list = [[] for _ in range(n+1)]
+        adj_list = [[] for v in range(n+1)]
         
         def build_graph():
-            for (u, v, weight) in times:
-                adj_list[u].append((v, weight))
-            return
+            for (src, dest, weight) in times:
+                adj_list[src].append((dest, weight))
+        
+        # Build the graph
+        build_graph()
+        
+        dist = [math.inf] * (n+1)
+        dist[0] = 0
+        
+        captured = [-1] * (n+1) # Stores whether a given vertex has been captured
+        captured[0] = 1
+    
         
         def dijkstra(src):
-            pq = []
-            captured = set([src])
+            # Insert all the neighbors of the src node into a min heap
+            min_heap = []
+            heappush(min_heap, (0, src))
             
-            # Insert all the adj vertices from src to pq
-            for (nei, weight) in adj_list[src]:
-                dist[nei] = weight
-                heappush(pq, (dist[nei], src, nei))
-                
-                
-            while pq:
-                weight, parent, curr = heappop(pq)
-                if curr in captured:
-                    continue
-                    
-                captured.add(curr)
-                
-                for (nei, weight) in adj_list[curr]:
-                    if nei not in captured:
-                        dist[nei] = min(dist[nei], dist[curr] + weight)
-                        heappush(pq, (dist[nei], curr, nei))
-                
-            
-        build_graph()
-        dijkstra(k)
-        max_dist = max(dist)
+            while min_heap:
+                (shortest_dist, node) = heappop(min_heap)
+                # Only update the shortest dist value for a node only if not captured before
+                if captured[node] == -1:
+                    dist[node] = shortest_dist
+                    captured[node] = 1
+
+                    for (nei, nweight) in adj_list[node]:
+                        if captured[nei] == -1:
+                            heappush(min_heap, (dist[node] + nweight, nei))
+                        
         
-        # Edge case: not all nodes reachable -> no valid path from k to all nodes
-        if max_dist == math.inf:
+        dijkstra(k)
+
+        
+        min_time = max(dist)
+        if min_time == math.inf:
             return -1
-        return max_dist
+        
+        return min_time
+    
