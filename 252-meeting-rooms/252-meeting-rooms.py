7d1341aff095b15461cd77bf0964112fb6957783
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-22 11:58:20 -0400

    Time: 142 ms (20.18%), Space: 17.3 MB (78.98%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,23 +1,20 @@
 class Solution:
-    # Time = O(NlogN)
+    # Time = O(nlogn)
     # Space = O(1)
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        N = len(intervals)
-        # Edge case: no intervals
-        if N == 0:
+        n = len(intervals)
+        # Edge case: empty intervals
+        if n == 0:
             return True
         
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key=lambda x: x[0])
+        prev_interval = intervals[0]
         
-        # Interval line sweep
-        for i in range(N):
-            if i == N-1:
-                next_start = math.inf
-            else:
-                next_start = intervals[i+1][0]
-            
-            # Check overlap
-            if intervals[i][1] > next_start:
+        for i in range(1, n):
+            # Overlap
+            if intervals[i][0] < prev_interval[1]:
                 return False
+            prev_interval = intervals[i]
         
+        # No overlap
         return True
