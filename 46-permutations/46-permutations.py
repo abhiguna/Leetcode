60acf1ae4fb00b6e08a3c0371cfbf118c312a019
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-22 12:03:56 -0400

    Time: 61 ms (45.63%), Space: 14.1 MB (57.59%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,25 +1,22 @@
 class Solution:
-    # Time = O(N*N!)
-    # Space = O(N)
+    # Time = O(n*n!), n: len(nums)
+    # Space = O(n)
     def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         res = []
-        N = len(nums)
         
-        def helper(fixed_idx):
-            # Base case
-            if fixed_idx == N:
+        def helper(idx):
+            # Base case:
+            if idx == n:
                 res.append(nums[:])
                 return
-            
-            # Recursive case
-            for i in range(fixed_idx, N):
+            # General case:
+            for i in range(idx, n):
                 # Swap
-                (nums[i], nums[fixed_idx]) = (nums[fixed_idx], nums[i])
-                helper(fixed_idx + 1)
+                nums[idx], nums[i] = nums[i], nums[idx]
+                helper(idx+1)
                 # Unswap
-                (nums[i], nums[fixed_idx]) = (nums[fixed_idx], nums[i])
-            
-            return
-        
+                nums[idx], nums[i] = nums[i], nums[idx]
+                
         helper(0)
         return res
