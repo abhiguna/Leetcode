de41f46275718bb75ed56cb12faf6c7fcb56f30e
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-23 10:19:12 -0400

    Time: 71 ms (11.11%), Space: 13.9 MB (92.78%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,42 +1,60 @@
 class Solution:
-    # Time = O(M), M: total num chars in words list
-    # Space = O(M)
+    # Time = O(m*n), m: avg length of each word, n: len(words)
+    # Space = O(m*n)
     def alienOrder(self, words: List[str]) -> str:
-        adj_list = {c: set() for w in words for c in w}
+        # Initialize adj_list and in_deg
+        adj_list = {c: set() for word in words for c in word}
+        in_deg = {c: 0 for c in adj_list.keys()}
+        
         def build_graph():
             for i in range(len(words) - 1):
-                w1 = words[i]
-                w2 = words[i+1]
-                min_len = min(len(w1), len(w2))
-                # Check invalid ordering -> same prefix but w2 has smaller len than w1
-                if w1[:min_len] == w2[:min_len] and len(w2) < len(w1):
-                    return True
+                word = words[i]
+                next_word = words[i+1]
+                
+                # Edge case: same prefix but len(word) > len(word_2) 
+                #     -> invalid lex. order
+                min_len = min(len(word), len(next_word))
+                if (word[:min_len]==next_word[:min_len]) and \
+                    (len(word) > len(next_word)):
+                    return False
+                
                 for j in range(min_len):
-                    # Found the first differing character
-                    if w1[j] != w2[j]:
-                        adj_list[w1[j]].add(w2[j])
+                    # Check if differs
+                    if word[j] != next_word[j]:
+                        # Edge case: skip duplicates
+                        if next_word[j] not in adj_list[word[j]]:
+                            adj_list[word[j]].add(next_word[j])
+                            in_deg[next_word[j]] += 1
                         break
-            return False
-        
-        is_invalid = build_graph()
-        if is_invalid: return ""
-        
-        # --- Use DFS with visited hashmap to track backEdges
-        alien_order = []
-        visited = {}
-        def dfs(src):
-            if src in visited:
-                return visited[src]
-            visited[src] = True
-            for nei in adj_list[src]:
-                has_cycle = dfs(nei)
-                if has_cycle: return True
-            visited[src] = False
-            alien_order.append(src)
-        
-        for c in adj_list:
-            has_cycle = dfs(c)
-            if has_cycle: return ""
-        alien_order.reverse()
-        return "".join(alien_order)
+                
+            return True
             
+        # Build graph
+        valid_input = build_graph()
+        if not valid_input:
+            return ""
+        
+        topsort = []
+        
+        # Topological sort
+        queue = deque()
+        for key, val in in_deg.items():
+            if val == 0:
+                queue.append(key)
+        
+        while queue:
+            node = queue.popleft()
+            topsort.append(node)
+            
+            for nei in adj_list[node]:
+                in_deg[nei] -= 1
+                if in_deg[nei] == 0:
+                    queue.append(nei)
+        
+        # Check if cycle exists
+        if len(topsort) < len(in_deg.keys()):
+            return ""
+        
+        return "".join(topsort)
+                    
+                    
