1f3b2699a2ca49f83f9d19ad9442ee5917a3a3ef
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-23 19:36:25 -0400

    Time: 101 ms (69.16%), Space: 14 MB (70.61%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,37 +1,40 @@
 class Solution:
-    # Time = O(N^(T/M)+1)
-    # Space = O(T/M)
+    # Time = O(n*T^n), T: target
+    # Space = O(max(n, T))
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        N = len(candidates)
-        T = target
+        n = len(candidates)
         res = []
         
-        def helper(idx, combination, target_left):
-            # Base case
+        def helper(idx, target_left, slate):
+            # Backtracking case
+            if target_left < 0:
+                return
+            # Base cases
             if target_left == 0:
-                res.append(combination[:])
+                res.append(slate[:])
+                return
+            if idx == n:
+                if target_left == 0:
+                    res.append(slate[:])
                 return
             
-            if idx == N or target_left < 0:
-                return
-            
-            # Recursive Case
+            # General case
             count = target_left // candidates[idx]
             
             # Exclude case
-            helper(idx+1, combination, target_left)
+            helper(idx + 1, target_left, slate)
             
             # Include case
             for i in range(1, count+1):
-                for j in range(1, i+1):
-                    combination.append(candidates[idx])
-                    target_left -= candidates[idx]
-                helper(idx+1, combination, target_left)
-                for j in range(1, i+1):
-                    combination.pop()
-                    target_left += candidates[idx]
-                    
-            return
+                slate.append(candidates[idx])
+                target_left -= candidates[idx]
+                helper(idx+1, target_left, slate)
+            
+            for i in range(count):
+                slate.pop()
+                target_left += candidates[idx]
         
-        helper(0, [], target)
+        helper(0, target, [])
         return res
+        
+        
