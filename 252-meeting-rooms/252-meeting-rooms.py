089af37625c1f5f3b65d1d603ab464b672276697
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-23 20:57:40 -0400

    Time: 101 ms (61.71%), Space: 17.5 MB (13.10%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -3,18 +3,21 @@ class Solution:
     # Space = O(1)
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         n = len(intervals)
-        # Edge case: empty intervals
+        
+        # Edge case: n == 0
         if n == 0:
             return True
         
-        intervals.sort(key=lambda x: x[0])
+        # Sort by start time
+        intervals.sort(key = lambda x: x[0])
+        
         prev_interval = intervals[0]
         
         for i in range(1, n):
-            # Overlap
+            # Check overlap
             if intervals[i][0] < prev_interval[1]:
                 return False
-            prev_interval = intervals[i]
+            
+            prev_interval[1] = max(prev_interval[1], intervals[i][1])
         
-        # No overlap
         return True
