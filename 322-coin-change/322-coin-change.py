7b3cf7762206c154000b59d8dc1badc2f2c6d9e6
Abhishek Gunasekar <agunase@purdue.edu>
2022-06-29 07:27:15 -0400

    Time: 1950 ms (58.95%), Space: 14.2 MB (52.38%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,13 +1,16 @@
+"""
+f(a) = min coins needed to satisfy a
+f(a) = 1 + min(f(a-c1), f(a-c2), f(a-c3)) 
+"""
 class Solution:
-    # Time = O(C*A), C: len(coins), A: amount
+    # Time = O(A*C), A: amount, C: len(coins)
     # Space = O(A)
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Edge case
+        # Edge case: amount = 0
         if amount == 0:
             return 0
         
-        # Initialize
-        table = [math.inf] * (amount + 1)
+        table = [math.inf] * (amount+1)
         table[0] = 0
         
         for a in range(1, amount+1):
@@ -15,7 +18,7 @@ class Solution:
                 if c <= a:
                     table[a] = min(table[a], 1 + table[a-c])
         
-        # Not possible to form amount
+        # Edge case: unable to make amount
         if table[amount] == math.inf:
             return -1
         
