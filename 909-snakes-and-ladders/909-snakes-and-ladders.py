99729201c9a51c233ce56fa9f3f739b77ca3a3d8
Abhishek Gunasekar <agunase@purdue.edu>
2022-07-23 17:44:22 -0400

    Time: 169 ms (61.56%), Space: 13.9 MB (66.34%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.py
+++ 909-snakes-and-ladders/909-snakes-and-ladders.py
@@ -1,38 +1,53 @@
 class Solution:
+    # Time = O(n^2), n: dimension of board
+    # Space = O(n^2)
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        size = len(board)
-        maxsquare = size*size
+        n = len(board)
+        goal = n*n
         
-        def numtorowcol(n):
-            row = (maxsquare - n) // size
-            c = (n-1) % (2*size) # c is in the range of 0 to 2*size - 1
-            if c < size:
+        def get_row_col(pos):
+            row = (goal - pos) // n
+            c = (pos-1) % (2*n)
+            if c < n:
                 col = c
             else:
-                col = 2*size - 1 - c
+                col = 2*n - 1 - c
             return (row, col)
-            
         
-        def minmoves(n):
-            q = collections.deque([n])
+        def min_moves(start):
+            # Stores the min distance to a cell
             visited = {}
-            visited[n] = 0 # Records the shortest path distance from the source to this vertex
-            while len(q) != 0:
-                curr = q.popleft()
+            
+            # BFS
+            queue = deque()
+            queue.append(start)
+            visited[start] = 0
+            
+            while queue:
+                curr_pos = queue.popleft()
+                
                 for i in range(1, 7):
-                    nxt = curr + i
-                    if nxt > maxsquare:
+                    next_pos = i + curr_pos
+                    
+                    # Out of bounds
+                    if next_pos > goal:
                         continue
-                    (r, c) = numtorowcol(nxt)
+                    
+                    (r, c) = get_row_col(next_pos)
+                    
+                    # Snake / Ladder
                     if board[r][c] != -1:
-                        nxt = board[r][c]
-                    if nxt not in visited:
-                        q.append(nxt)
-                        visited[nxt] = visited[curr] + 1
-                        if nxt == maxsquare:
-                            return visited[nxt]
+                        next_pos = board[r][c]
+                    
+                    # Update queue
+                    if next_pos not in visited:
+                        visited[next_pos] = 1 + visited[curr_pos]
+                        queue.append(next_pos)
+                        if next_pos == goal:
+                            return visited[next_pos]
+        
+            # Not possible to reach goal square
             return -1
         
+        return min_moves(1)
         
-        
-        return minmoves(1)
