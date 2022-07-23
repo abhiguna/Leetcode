4042a2475ab8c806918de02d8d83e9dd0b243188
Abhishek Gunasekar <agunase@purdue.edu>
2022-07-23 18:46:08 -0400

    Time: 105 ms (92.82%), Space: 17.1 MB (42.40%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,39 +1,50 @@
 class Solution:
     # Time = O(m+n), m: len(prerequisites), n: numCourses
     # Space = O(m+n)
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = [[] for v in range(numCourses)]
-        in_deg = {v: 0 for v in range(numCourses)}
+        arrival = [-1] * numCourses
+        departure = [-1] * numCourses
+        timestamp = [0]
+        course_list = []
         
+        # Build the graph
         def build_graph():
             for (course, pre) in prerequisites:
                 adj_list[pre].append(course)
-                in_deg[course] += 1
+            
         
-        # Build graph
         build_graph()
         
-        # Kahn's topological sorting algo.
-        res = []
-        queue = deque()
-        
-        for (course, num_pre) in in_deg.items():
-            if num_pre == 0:
-                res.append(course)
-                queue.append(course)
-        
-        while queue:
-            course = queue.popleft()
+        # DFS -> return whether cycle exists
+        def dfs(node):
+            arrival[node] = timestamp[0]
+            timestamp[0] += 1
             
-            for ncourse in adj_list[course]:
-                in_deg[ncourse] -= 1
-                if in_deg[ncourse] == 0:
-                    queue.append(ncourse)
-                    res.append(ncourse)
+            for nei in adj_list[node]:
+                if arrival[nei] == -1:
+                    has_cycle = dfs(nei)
+                    if has_cycle:
+                        return True
+                else:
+                    # Back edge 
+                    if departure[nei] == -1:
+                        return True
+            
+            # Add the course to the course list before departing
+            course_list.append(node)
+            departure[node] = timestamp[0]
+            timestamp[0] += 1
+            return False
+            
         
-        # Check for cycle
-        if len(res) < numCourses:
-            return []
+        # Outer loop -> traverse all components
+        for v in range(numCourses):
+            if arrival[v] == -1:
+                has_cycle = dfs(v)
+                if has_cycle:
+                    return []
         
-        return res
-        
+        # Course order must be in decreasing order of departure times
+        course_list.reverse()
+        return course_list
