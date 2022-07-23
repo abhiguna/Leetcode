cbb3e97c9fe1f1d9c265293d3dc8ebf96ed7fb73
Abhishek Gunasekar <agunase@purdue.edu>
2022-07-23 18:40:25 -0400

    Time: 159 ms (48.07%), Space: 17.1 MB (25.63%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,39 +1,45 @@
 class Solution:
-    # Kahn's Topological Sort 
-    # Time = O(M+N)
-    # Space = O(M+N)
+    # Time = O(m+n), m: len(prerequisites), n: numCourses
+    # Space = O(m+n)
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Build the in-degree of each node
-        in_deg = {v:0 for v in range(numCourses)}
         adj_list = [[] for v in range(numCourses)]
+        arrival = [-1] * numCourses
+        departure = [-1] * numCourses
+        timestamp = [0]
         
-        for (c, pre) in prerequisites:
-            adj_list[pre].append(c)
-            in_deg[c] += 1
+        # Build the graph
+        def build_graph():
+            for (course, pre) in prerequisites:
+                adj_list[pre].append(course)
+            
         
-        # Add all the nodes with in-deg of 0 to a queue
-        queue = deque()
-        for c in in_deg:
-            if in_deg[c] == 0:
-                queue.append(c)
+        build_graph()
         
-        topsort = []
-        while queue:
-            node = queue.popleft()
-            topsort.append(node)
+        # DFS -> return whether cycle exists
+        def dfs(node):
+            arrival[node] = timestamp[0]
+            timestamp[0] += 1
             
             for nei in adj_list[node]:
-                in_deg[nei] -= 1
-                if in_deg[nei] == 0:
-                    queue.append(nei)
-        
-        if len(topsort) < numCourses:
+                if arrival[nei] == -1:
+                    has_cycle = dfs(nei)
+                    if has_cycle:
+                        return True
+                else:
+                    # Back edge 
+                    if departure[nei] == -1:
+                        return True
+            
+            departure[node] = timestamp[0]
+            timestamp[0] += 1
             return False
-
-        return True
             
         
+        # Outer loop -> traverse all components
+        for v in range(numCourses):
+            if arrival[v] == -1:
+                has_cycle = dfs(v)
+                if has_cycle:
+                    return False
         
-        
-        
-        
+        return True
