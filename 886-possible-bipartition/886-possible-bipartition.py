76f8d38a35bf2b3bf966015c478336780f4454a8
Abhishek Gunasekar <agunase@purdue.edu>
2022-07-23 10:48:51 -0400

    Time: 1119 ms (41.85%), Space: 21.9 MB (36.98%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.py
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -1,51 +1,46 @@
 class Solution:
-    # Time = O(M+N)
-    # Space = O(M+N)
+    # Time = O(m+n), m: len(dislikes), n: # of people
+    # Space = O(m+n)
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        adj_list = [[] for i in range(n+1)]
+        group_map = {
+            "A": "B",
+            "B": "A"
+        }
+        adj_list = [[] for v in range(n+1)]
         visited = [-1] * (n+1)
         parent = [-1] * (n+1)
-        level = [-1] * (n+1)
+        group = [-1] * (n+1)
         
         def build_graph():
             for (src, dest) in dislikes:
                 adj_list[src].append(dest)
                 adj_list[dest].append(src)
-            return
-        
-        def bfs(src):
-            queue = deque()
-            queue.append(src)
-            visited[src] = 1
-            level[src] = 0
             
-            while queue:
-                node = queue.popleft()
-                
-                for nei in adj_list[node]:
-                    if visited[nei] == -1:
-                        visited[nei] = 1
-                        parent[nei] = node
-                        level[nei] = 1 + level[node]
-                        queue.append(nei)
-                    else:
-                        # Cross edge
-                        if parent[node] != nei:
-                            # Has odd len cycle
-                            if level[node] == level[nei]:
-                                return True
-            
-            return False
-            
-        
         build_graph()
         
-        for v in range(1, n+1):
+        # DFS -> check group separation in a component
+        def dfs(node, g):
+            visited[node] = 1
+            group[node] = g
+            
+            for nei in adj_list[node]:
+                if visited[nei] == -1:
+                    can_split = dfs(nei, group_map[g])
+                    if not can_split:
+                        return False
+                else:
+                    # Back edge
+                    if nei != parent[node]:
+                        if group[nei] == group[node]:
+                            return False
+            
+            return True
+        
+        # Outer Loop -> check all components
+        for v in range(n):
             if visited[v] == -1:
-                has_odd_len_cycle = bfs(v)
-                if has_odd_len_cycle:
+                can_split = dfs(v, "A")
+                if not can_split:
                     return False
         
         return True
-        
-        
